5c655e0f8ea8c0fedf7e4d3d04387126c745eb5d
angie <angheloalf95@gmail.com>
2023-03-24 14:53:00 -0300

    global.h

---
+++ include/global.h
@@ -0,0 +1,16 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include "ultra64.h"
+#include "attributes.h"
+#include "alignment.h"
+#include "other_types.h"
+#include "unk.h"
+#include "unknown_structs.h"
+#include "boot_functions.h"
+#include "boot_variables.h"
+#include "code_functions.h"
+#include "code_variables.h"
+#include "macros.h"
+
+#endif
