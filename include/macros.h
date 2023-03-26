b07ed48939a888504c646512ab70809e2ba1a443
angie <angheloalf95@gmail.com>
2023-03-26 10:59:11 -0300

    yeet saved_regs

--- include/macros.h
+++ include/macros.h
@@ -17,17 +17,4 @@
 #define ARRAY_COUNTU(arr) (u32)(sizeof(arr) / sizeof(arr[0]))
 
 
-#define saved_reg_s0 0
-#define saved_reg_s1 0
-#define saved_reg_s2 0
-#define saved_reg_s3 0
-#define saved_reg_s4 0
-#define saved_reg_s5 0
-#define saved_reg_s6 0
-#define saved_reg_s7 0
-#define bitwise
-#define saved_reg_f20 0.0f
-#define saved_reg_f22 0.0f
-
-
 #endif
