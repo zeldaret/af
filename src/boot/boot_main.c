39a2e453bb882b2498c7c4aa9073a9f85d048f0c
Anghelo Carvajal <angheloalf95@gmail.com>
2023-05-08 12:33:08 -0400

    Do not use `undefined_syms_auto` files when linking (#15)
    * remove some symbols
    
    * Adding more symbols to undefined_syms.ld
    
    * Avoid using the undefined_auto completely
    
    * relocs for entry.s

--- src/boot/boot_main.c
+++ src/boot/boot_main.c
@@ -2,4 +2,4 @@
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/boot_main/func_80025C60_jp.s")
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/boot_main/func_80025CC0_jp.s")
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/boot_main/bootproc.s")
