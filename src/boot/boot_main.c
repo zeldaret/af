22e55a36a2128f3160c9ef1644214f5e6e826845
Derek Hensley <hensley.derek58@gmail.com>
2023-07-16 10:02:44 -0700

    Some cleanup

--- src/boot/boot_main.c
+++ src/boot/boot_main.c
@@ -2,11 +2,12 @@
 #include "ultra64.h"
 
 #include "idle.h"
+#include "m_thread.h"
+#include "segment_symbols.h"
 #include "stack.h"
 #include "stackcheck.h"
 
-extern u8 dmadata_VRAM[];
-extern OSPiHandle* gCartHandle;
+extern OSPiHandle* gCartHandle; // TODO: Determine where this goes
 
 STACK(sIdleStack, 0x400);
 StackEntry sIdleStackInfo;
@@ -14,33 +15,39 @@ StackEntry sBootStackInfo;
 OSThread sIdleThread;
 STACK(sBootStack, 0x400);
 
-void func_80025C60_jp(void) {
-    s32 size = (uintptr_t)func_80025C60_jp + 0x7FFFFC00;
+// Unknown original name
+void bootclear(void) {
+    s32 size = (uintptr_t)bootclear - BOOT_ADDRESS_ULTRA;
     
     if (size > 0) {
-        bzero(0x80000400, size);
+        bzero((void*)BOOT_ADDRESS_ULTRA, size);
     }
     
-    size = osMemSize - OS_K0_TO_PHYSICAL(dmadata_VRAM);
+    size = osMemSize - OS_K0_TO_PHYSICAL(SEGMENT_VRAM_START(dmadata));
     if (size > 0) {
-        bzero(dmadata_VRAM, size);
+        bzero(SEGMENT_VRAM_START(dmadata), size);
     }
 }
 
 void bootproc(void) {
     StackCheck_Init(&sBootStackInfo, sBootStack, STACK_TOP(sBootStack), 0, -1, "boot");
-    osMemSize = 0x400000;
-    __osInitialize_common();
+
+    osMemSize = 0x400000; // 4MB
+    osInitialize();
+
     if (osAppNMIBuffer[15] & 4) {
         osAppNMIBuffer[15] &= ~4;
         osAppNMIBuffer[15] |= 2;
     } else {
         osAppNMIBuffer[15] &= ~2;
     }
+
     osUnmapTLBAll();
-    func_80025C60_jp();
+    bootclear();
+
     gCartHandle = osCartRomInit();
+
     StackCheck_Init(&sIdleStackInfo, sIdleStack, STACK_TOP(sIdleStack), 0, 0x100, "idle");
-    osCreateThread(&sIdleThread, 1, Idle_ThreadEntry, NULL, STACK_TOP(sIdleStack), 0xC);
+    osCreateThread(&sIdleThread, M_THREAD_ID_IDLE, Idle_ThreadEntry, NULL, STACK_TOP(sIdleStack), M_PRIORITY_IDLE);
     osStartThread(&sIdleThread);
 }
