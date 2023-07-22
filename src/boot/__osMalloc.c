c8949ee59ab6fda9bae5ba8a3eb189f130ecb972
angie <angheloalf95@gmail.com>
2023-07-22 13:10:06 -0400

    start

---
+++ src/boot/__osMalloc.c
@@ -0,0 +1,36 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/arena_lock_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/arena_lock.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/arena_unlock.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/search_last_block.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osMallocInit.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osMallocAddBlock.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osMallocCleanup.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osMallocIsInitalized.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osMallocNoLock.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osMalloc.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osMallocR.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osFree_NoLock.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osFree.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osRealloc.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osGetFreeArena.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/ArenaImpl_FaultClient.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osCheckArena.s")
