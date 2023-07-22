0fdb91798745d2a6dd2f1672bd89b14a83bfa640
angie <angheloalf95@gmail.com>
2023-07-22 13:58:09 -0400

    arena_lock_init, __osMallocInit, __osMallocAddBlock and __osMallocCleanup

--- include/osmalloc.h
+++ include/osmalloc.h
@@ -3,13 +3,32 @@
 
 #include "ultra64.h"
 
-// void arena_lock_init();
-// void arena_lock();
-// void arena_unlock();
-// void search_last_block();
-// void __osMallocInit();
-// void __osMallocAddBlock();
-// void __osMallocCleanup();
+// TODO: check size and members
+typedef struct ArenaNode {
+    /* 0x0 */ s16 magic; // Should always be 0x7373
+    /* 0x2 */ s16 isFree;
+    /* 0x4 */ size_t size;
+    /* 0x8 */ struct ArenaNode* next;
+    /* 0xC */ struct ArenaNode* prev;
+} ArenaNode; // size = 0x10
+
+// TODO: check size and members
+typedef struct {
+    /* 0x00 */ ArenaNode* head;
+    /* 0x04 */ void* start;
+    /* 0x08 */ OSMesgQueue lock;
+    /* 0x20 */ u8 unk20;
+    /* 0x21 */ u8 isInit;
+    /* 0x22 */ u8 flag;
+} Arena; // size = 0x24
+
+void arena_lock_init(Arena* arena);
+void arena_lock(Arena* arena);
+void arena_unlock(Arena* arena);
+ArenaNode* search_last_block(Arena* arena);
+void __osMallocInit(Arena* arena, void* heap, size_t size);
+void __osMallocAddBlock(Arena* arena, void* heap, size_t size);
+void __osMallocCleanup(Arena* arena);
 // void __osMallocIsInitalized();
 // void __osMallocNoLock();
 // void __osMalloc();
--- src/boot/__osMalloc.c
+++ src/boot/__osMalloc.c
@@ -1,7 +1,29 @@
-#include "global.h"
+#include "osmalloc.h"
+#include "alignment.h"
+#include "libc/stdbool.h"
+#include "libc/stddef.h"
+#include "libc/stdint.h"
+#include "macros.h"
 
+//#define FILL_ALLOCBLOCK (1 << 0)
+//#define FILL_FREEBLOCK (1 << 1)
+//#define CHECK_FREE_BLOCK (1 << 2)
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/arena_lock_init.s")
+#define NODE_MAGIC (0x7373)
+
+//#define BLOCK_UNINIT_MAGIC (0xAB)
+//#define BLOCK_UNINIT_MAGIC_32 (0xABABABAB)
+//#define BLOCK_ALLOC_MAGIC (0xCD)
+//#define BLOCK_ALLOC_MAGIC_32 (0xCDCDCDCD)
+//#define BLOCK_FREE_MAGIC (0xEF)
+//#define BLOCK_FREE_MAGIC_32 (0xEFEFEFEF)
+
+// sArenaLockMsg
+extern OSMesg B_80041A00_jp[1];
+
+void arena_lock_init(Arena* arena) {
+    osCreateMesgQueue(&arena->lock, B_80041A00_jp, ARRAY_COUNT(B_80041A00_jp));
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/arena_lock.s")
 
@@ -9,11 +31,60 @@
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/search_last_block.s")
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osMallocInit.s")
+void __osMallocInit(Arena* arena, void* heap, size_t size) {
+    bzero(arena, sizeof(Arena));
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osMallocAddBlock.s")
+    arena_lock_init(arena);
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osMallocCleanup.s")
+    __osMallocAddBlock(arena, heap, size);
+    arena->isInit = true;
+}
+
+void __osMallocAddBlock(Arena* arena, void* heap, size_t size) {
+    ptrdiff_t diff;
+    s32 alignedSize;
+    ArenaNode *firstNode;
+    ArenaNode* lastNode;
+
+    if (heap == NULL) {
+        return;
+    }
+
+    firstNode = (ArenaNode*)ALIGN16((uintptr_t)heap);
+    diff = (uintptr_t)firstNode - (uintptr_t)heap;
+    alignedSize = ((s32)size - diff) & ~0xF;
+
+    // If the size of the heap is smaller than sizeof(ArenaNode), then the initialization will silently fail
+    if (alignedSize <= (s32)sizeof(ArenaNode)) {
+        return;
+    }
+
+    firstNode->next = NULL;
+    firstNode->prev = NULL;
+    firstNode->size = alignedSize - sizeof(ArenaNode);
+    firstNode->isFree = true;
+    firstNode->magic = NODE_MAGIC;
+
+    arena_lock(arena);
+
+    lastNode = search_last_block(arena);
+
+    // Checks if there's already a block
+    if (lastNode == NULL) {
+        arena->head = firstNode;
+        arena->start = heap;
+    } else {
+        // Chain the existing block with the new one
+        firstNode->prev = lastNode;
+        lastNode->next = firstNode;
+    }
+
+    arena_unlock(arena);
+}
+
+void __osMallocCleanup(Arena* arena) {
+    bzero(arena, sizeof(Arena));
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/__osMalloc/__osMallocIsInitalized.s")
 
