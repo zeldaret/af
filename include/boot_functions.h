2a4cf43ff654df7550e370c1ed7a04a6cae5a003
angie <angheloalf95@gmail.com>
2023-07-23 11:14:35 -0400

    memmove comment

--- include/boot_functions.h
+++ include/boot_functions.h
@@ -28,7 +28,7 @@
 // void func_8003B9B0_jp();
 // void func_8003B9E0_jp();
 // void func_8003BA20_jp();
-void* func_8003BA60_jp(void* dst, void* src, size_t size); // __osMemcpy
+void* func_8003BA60_jp(void* dst, void* src, size_t size); // MM: __osMemcpy, gamecube refers to it as memmove
 // void func_8003BAF0_jp();
 
 
