462cd091eeff35bb95fcf1fedfd24dff7ba038b3
Tom Overton <tom-overton@users.noreply.github.com>
2023-08-03 07:39:54 -0700

    m_common_data OK (#27)
    * m_common_data OK
    
    * Respond to review

---
+++ src/code/m_common_data.c
@@ -0,0 +1,26 @@
+#include "global.h"
+#include "m_common_data.h"
+#include "m_field_info.h"
+#include "68AAF0.h"
+
+CommonData common_data;
+
+void common_data_reinit(void) {
+    u8 sp1F = common_data.unk_10AB0;
+
+    bzero(&common_data, sizeof(CommonData));
+    common_data.unk_1014B = -1;
+    common_data.unk_10000 = 1;
+    common_data.unk_10004 = -1;
+    common_data.unk_107B6 = 0xC9;
+    common_data.unk_10AB0 = sp1F;
+    mFRm_ClearSaveCheckData(&common_data);
+}
+
+void common_data_init(void) {
+    common_data_reinit();
+}
+
+void common_data_clear(void) {
+    clip_clear();
+}
