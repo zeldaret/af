06789e3f62914162a19d68736384ba07201cb1f1
Prakxo <lycos2003@gmail.com>
2023-08-04 20:24:18 +0200

    refactor 5

--- include/irqmgr.h
+++ include/irqmgr.h
@@ -10,28 +10,28 @@
 #define IRQ_PRENMI500_MSG 673
 
 typedef struct {
-  /* 0x00 */ s16 type;
-  /* 0x02 */ s8 data[30];
+    /* 0x00 */ s16 type;
+    /* 0x02 */ s8 data[30];
 } IrqmgrMsg; //size = 0x20
 
 
 typedef struct IrqmgrClient {
-   /* 0x0 */ struct IrqmgrClient* next;
-   /* 0x4 */ OSMesgQueue* msgQueue;
+    /* 0x0 */ struct IrqmgrClient* next;
+    /* 0x4 */ OSMesgQueue* msgQueue;
 } IrqmgrClient; // size = 0x8
 
 typedef struct {
-  /* 0x000 */ IrqmgrMsg msgRetrace;
-  /* 0x020 */ IrqmgrMsg msgPreNMI;
-  /* 0x040 */ IrqmgrMsg msgDelayPreNMI;
-  /* 0x060 */ OSMesgQueue _msgQueue;
-  /* 0x078 */ OSMesg msgBuf[8];
-  /* 0x098 */ OSThread thread;
-  /* 0x248 */ IrqmgrClient* clients;
-  /* 0x24C */ u8 prenmi;
-  /* 0x250 */ OSTime prenmiTime;
-  /* 0x258 */ OSTimer timer;
-  /* 0x278 */ OSTime retraceTime;
+    /* 0x000 */ IrqmgrMsg msgRetrace;
+    /* 0x020 */ IrqmgrMsg msgPreNMI;
+    /* 0x040 */ IrqmgrMsg msgDelayPreNMI;
+    /* 0x060 */ OSMesgQueue _msgQueue;
+    /* 0x078 */ OSMesg msgBuf[8];
+    /* 0x098 */ OSThread thread;
+    /* 0x248 */ IrqmgrClient* clients;
+    /* 0x24C */ u8 prenmi;
+    /* 0x250 */ OSTime prenmiTime;
+    /* 0x258 */ OSTimer timer;
+    /* 0x278 */ OSTime retraceTime;
 } Irqmgr; // size = 0x280
 
 void irqmgr_AddClient(IrqmgrClient* client, OSMesgQueue* msgQueue);
