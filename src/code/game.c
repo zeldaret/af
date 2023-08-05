9ec3c171dacf7ebd8a89b24355120a3222eab29f
angie <angheloalf95@gmail.com>
2023-08-05 11:37:17 -0400

    C file

---
+++ src/code/game.c
@@ -0,0 +1,57 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/func_800D2E00_jp.s")
+
+const u16 RO_80117CE0_jp[] = {
+    0xFFC1,
+    0xFFC1,
+    0xFFC1,
+    0xFFC1,
+    0x7BDF,
+    0x7BDF,
+    0x07FF,
+    0xF83F,
+    0x7BDF,
+    0x7BDF,
+    0x7BDF,
+    0x7BDF,
+    0xF801,
+    0x7BDF,
+    0x07C1,
+    0x003F,
+};
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/func_800D2E58_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_debug_draw_last.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_draw_first.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_draw_last.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_get_controller.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/SetGameFrame.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_main.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_init_hyral.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_resize_hyral.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_ct.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_dt.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_get_next_game_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_get_next_game_class_size.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_is_doing.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_getFreeBytes.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_goto_next_game_play.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/game/game_goto_next_game_name_famicom_emu.s")
