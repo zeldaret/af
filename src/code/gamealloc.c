437b1cec88e69b7217f45f37c589e505fec1c715
angie <angheloalf95@gmail.com>
2023-08-05 12:46:16 -0400

    C file

---
+++ src/code/gamealloc.c
@@ -0,0 +1,12 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/gamealloc/func_800D3720_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/gamealloc/gamealloc_malloc.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/gamealloc/gamealloc_free.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/gamealloc/gamealloc_cleanup.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/gamealloc/gamealloc_init.s")
