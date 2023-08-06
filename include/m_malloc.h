6200b5f768421312ba1d7414e1be465e9806aad7
Angie <angheloalf95@gmail.com>
2023-08-06 18:01:18 -0400

    match

--- include/m_malloc.h
+++ include/m_malloc.h
@@ -3,15 +3,15 @@
 
 #include "ultra64.h"
 
-// void zelda_malloc();
+void *zelda_malloc(size_t size);
 void* zelda_malloc_r(size_t size);
-// void zelda_realloc();
-void* zelda_free(void* ptr);
-// void zelda_calloc();
-// void zelda_GetFreeArena();
-// void zelda_CheckArena();
-// void zelda_InitArena();
-// void zelda_CleanupArena();
+void *zelda_realloc(void* ptr, size_t size);
+void zelda_free(void* ptr);
+void* zelda_calloc(s32 num, size_t size);
+void zelda_GetFreeArena(size_t *outMaxFree, size_t *outFree, size_t *outAlloc);
+s32 zelda_CheckArena(void);
+void zelda_InitArena(void* heap, size_t size);
+void zelda_CleanupArena(void);
 s32 zelda_MallocIsInitalized(void);
 
 #endif
---
+++ src/code/m_malloc.c
@@ -0,0 +1,55 @@
+#include "m_malloc.h"
+#include "osmalloc.h"
+#include "fault.h"
+
+extern FaultClient B_80141FC8_jp;
+extern Arena zelda_arena;
+
+void *zelda_malloc(size_t size) {
+    return __osMalloc(&zelda_arena, size);
+}
+
+void* zelda_malloc_r(size_t size) {
+    return __osMallocR(&zelda_arena, size);
+}
+
+void *zelda_realloc(void* ptr, size_t size) {
+    __osRealloc(&zelda_arena, ptr, size);
+}
+
+void zelda_free(void* ptr) {
+    __osFree(&zelda_arena, ptr);
+}
+
+void* zelda_calloc(s32 num, size_t size) {
+    void* ptr;
+    u32 n = num * size;
+
+    ptr = __osMalloc(&zelda_arena, n);
+    if (ptr != NULL) {
+        bzero(ptr,  n);
+    }
+    return ptr;
+}
+
+void zelda_GetFreeArena(size_t *outMaxFree, size_t *outFree, size_t *outAlloc) {
+    __osGetFreeArena(&zelda_arena, outMaxFree, outFree, outAlloc);
+}
+
+s32 zelda_CheckArena(void) {
+    return __osCheckArena(&zelda_arena);
+}
+
+void zelda_InitArena(void* heap, size_t size) {
+    __osMallocInit(&zelda_arena, heap, size);
+    Fault_AddClient(&B_80141FC8_jp, (void *) ArenaImpl_FaultClient, &zelda_arena, NULL);
+}
+
+void zelda_CleanupArena(void) {
+    Fault_RemoveClient(&B_80141FC8_jp);
+    __osMallocCleanup(&zelda_arena);
+}
+
+s32 zelda_MallocIsInitalized(void) {
+    return __osMallocIsInitalized(&zelda_arena);
+}
