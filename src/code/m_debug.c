a313269701ed8d42a9c3e792db8f960559395a03
Prakxo <lycos2003@gmail.com>
2023-08-06 21:24:25 +0200

    m_debug OK

---
+++ src/code/m_debug.c
@@ -0,0 +1,21 @@
+#include "m_debug.h"
+#include "malloc.h"
+
+Debug_mode* debug_mode;
+
+void new_Debug_mode(){
+
+    s32 i;
+
+    debug_mode = (Debug_mode*)malloc(sizeof(Debug_mode));
+
+    debug_mode->mode = 0;
+    debug_mode->type = 0;
+    debug_mode->inputR = 0;
+    debug_mode->oldKey = 0;
+    debug_mode->keyWait = 0;
+
+    for(i = 0; i < DEBUG_REG_COUNT; i++){
+        debug_mode->r[i] = 0;
+    }
+}
