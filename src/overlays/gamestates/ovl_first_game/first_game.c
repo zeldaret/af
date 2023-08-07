c52fabc6a2cde1b354a6de514bf2e27fa63ff65f
Angie <angheloalf95@gmail.com>
2023-08-06 20:09:03 -0400

    c file

---
+++ src/overlays/gamestates/ovl_first_game/first_game.c
@@ -0,0 +1,9 @@
+#include "first_game.h"
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/gamestates/ovl_first_game/first_game/func_808029D0_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/gamestates/ovl_first_game/first_game/first_game_cleanup.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/gamestates/ovl_first_game/first_game/first_game_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/gamestates/ovl_first_game/first_game/func_80802A60_jp.s")
