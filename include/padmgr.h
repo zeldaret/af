bbede4c7f33a55e4eae04cfe947821021adc5f30
Prakxo <lycos2003@gmail.com>
2023-08-10 23:47:06 +0200

    zurumode OK

--- include/padmgr.h
+++ include/padmgr.h
@@ -4,6 +4,33 @@
 #include "ultra64.h"
 #include "libu64/pad.h"
 
+typedef struct padmgr{
+    /* 0x00 */ u8 _0;
+    /* 0x04 */ void (*rumbleRetraceCallback)(void*);
+    /* 0x08 */ void* rumbleRetraceArg;
+    /* 0x0C */ void (*inputRetraceCallback)(void*);
+    /* 0x10 */ void* inputRetraceArg;
+    /* 0x14 */ u8 _14[0x2B0 - 0x14];
+    /* 0x2B0 */ OSContPad pads[MAXCONTROLLERS];
+    /* 0x2C8 */ u8 _2C8[0x480 - 0x2C8];
+} Padmgr; // size = 0x480
+
+#define PADMGR_SET_RETRACE_CALLACK(callback, arg) \
+    do {                                                  \
+        padmgr_class.inputRetraceCallback = (callback);           \
+        padmgr_class.inputRetraceArg = (arg);             \
+    } while (0)
+
+#define PADMGR_UNSET_RETRACE_CALLACK(callback, param) \
+do { \
+  Padmgr* mgr = &padmgr_class; \
+  if (mgr->inputRetraceCallback == (callback) && mgr->inputRetraceArg == (param)) { \
+    mgr->inputRetraceCallback = NULL; \
+    mgr->inputRetraceArg = NULL; \
+  } \
+} while (0)
+
+
 // void padmgr_LockSerialMesgQ();
 // void padmgr_UnlockSerialMesgQ();
 // void padmgr_LockContData();
@@ -25,5 +52,7 @@ void padmgr_ClearPadData(Input inputs[4]);
 // void func_800D763C_jp();
 // void func_800D765C_jp();
 // void func_800D7678_jp();
+
+extern Padmgr padmgr_class;
 
 #endif
---
+++ src/code/zurumode.c
@@ -0,0 +1,206 @@
+#include "zurumode.h"
+#include "m_nmi_buf.h"
+#include "padmgr.h"
+
+zuruKeycheck zuruKeyCheck;
+
+s32 zurumode_flag = 0;
+
+s32 zurumode_update() {
+    s32 flag_now;
+
+    zurumode_flag = (APPNMI_ZURUMODE_GET()) ? ((APPNMI_ZURUMODE2_GET()) ? 2 : 1) : 0;
+
+    return zurumode_flag;
+}
+
+s32 zerucheck_init(zuruKeycheck* keycheck) {
+    keycheck->state = 0;
+    keycheck->progressing = 0;
+    keycheck->lastController = 0;
+}
+
+s32 zerucheck_key_check(zuruKeycheck* keycheck, u16 controller) {
+    u32 controller_now = controller;
+    s32 state = keycheck->state;
+    s32 progressing = keycheck->progressing;
+    u32 controller_new;
+
+    if (state != ZURUMODE_STAGE_FINAL) {
+        controller_new = (keycheck->lastController ^ controller_now) & controller_now;
+        keycheck->lastController = controller_now;
+
+        if ((controller == 0) && (progressing == 1)) {
+            progressing = 0;
+        } else if (controller_new != 0) {
+            if (progressing == 1) {
+                state = 0;
+            }
+            switch (state) {
+
+                case ZURUMODE_STAGE_0:
+
+                    if (controller_now == (R_TRIG | L_TRIG | Z_TRIG) && controller_new == Z_TRIG) {
+                        state = ZURUMODE_STAGE_1;
+                        progressing = 1;
+                    }
+                    break;
+
+                case ZURUMODE_STAGE_1:
+
+                    if (controller_new == U_JPAD) {
+                        state = ZURUMODE_STAGE_2;
+                    } else {
+                        state = ZURUMODE_STAGE_0;
+                    }
+
+                    break;
+
+                case ZURUMODE_STAGE_2:
+
+                    if (controller_new == D_CBUTTONS) {
+                        state = ZURUMODE_STAGE_3;
+                        progressing = 1;
+                    } else {
+                        state = ZURUMODE_STAGE_0;
+                    }
+                    break;
+
+                case ZURUMODE_STAGE_3:
+
+                    if (controller_new == U_CBUTTONS) {
+                        state = ZURUMODE_STAGE_4;
+                    } else {
+                        state = ZURUMODE_STAGE_0;
+                    }
+
+                    break;
+
+                case ZURUMODE_STAGE_4:
+
+                    if (controller_new == D_JPAD) {
+                        state = ZURUMODE_STAGE_5;
+                        progressing = 1;
+                    } else {
+                        state = ZURUMODE_STAGE_0;
+                    }
+
+                    break;
+
+                case ZURUMODE_STAGE_5:
+
+                    if (controller_new == L_JPAD) {
+                        state = ZURUMODE_STAGE_6;
+                    } else {
+                        state = ZURUMODE_STAGE_0;
+                    }
+                    break;
+
+                case ZURUMODE_STAGE_6:
+
+                    if (controller_new == L_CBUTTONS) {
+                        state = ZURUMODE_STAGE_7;
+                        progressing = 1;
+                    } else {
+                        state = ZURUMODE_STAGE_0;
+                    }
+
+                    break;
+
+                case ZURUMODE_STAGE_7:
+
+                    if (controller_new == R_CBUTTONS) {
+                        state = ZURUMODE_STAGE_8;
+                    } else {
+                        state = ZURUMODE_STAGE_0;
+                    }
+                    break;
+
+                case ZURUMODE_STAGE_8:
+
+                    if (controller_new == R_JPAD) {
+                        state = ZURUMODE_STAGE_9;
+                        progressing = 1;
+                    } else {
+                        state = ZURUMODE_STAGE_0;
+                    }
+
+                    break;
+
+                case ZURUMODE_STAGE_9:
+
+                    if (controller_new == (B_BUTTON | A_BUTTON)) {
+                        state = ZURUMODE_STAGE_10;
+                    } else if (controller_new == A_BUTTON) {
+                        state = ZURUMODE_STAGE_91;
+                    } else if (controller_new == B_BUTTON) {
+                        state = ZURUMODE_STAGE_92;
+                    } else {
+                        state = ZURUMODE_STAGE_0;
+                    }
+
+                    break;
+
+                case ZURUMODE_STAGE_91:
+
+                    if (controller_new == B_BUTTON) {
+                        state = ZURUMODE_STAGE_10;
+                    } else {
+                        state = ZURUMODE_STAGE_0;
+                    }
+
+                    break;
+
+                case ZURUMODE_STAGE_92:
+
+                    if (controller_new == A_BUTTON) {
+                        state = ZURUMODE_STAGE_10;
+                    } else {
+                        state = ZURUMODE_STAGE_0;
+                    }
+
+                    break;
+
+                case ZURUMODE_STAGE_10:
+
+                    if (controller_new == START_BUTTON) {
+                        state = ZURUMODE_STAGE_FINAL;
+                    } else {
+                        state = ZURUMODE_STAGE_0;
+                    }
+
+                    break;
+            }
+        }
+    }
+
+    keycheck->state = state;
+    keycheck->progressing = progressing;
+
+    return state == ZURUMODE_STAGE_FINAL;
+}
+
+void zurumode_callback(void* p) {
+
+    if (zerucheck_key_check(&zuruKeyCheck, (u32)(((Padmgr*)p)->pads[1].button))) {
+        zerucheck_init(&zuruKeyCheck);
+        APPNMI_ZURUMODE_FLP();
+        zurumode_update();
+        if ((u32)(((Padmgr*)p)->pads[0].button) & 0x80) {
+            APPNMI_ZURUMODE3_FLP();
+        }
+    }
+}
+
+void zurumode_init() {
+
+    zurumode_update();
+    zerucheck_init(&zuruKeyCheck);
+    PADMGR_SET_RETRACE_CALLACK(&zurumode_callback, &padmgr_class);
+}
+
+void zurumode_cleanup() {
+
+    PADMGR_UNSET_RETRACE_CALLACK(&zurumode_callback, &padmgr_class);
+    zurumode_flag = 0;
+}
