d9a77b4949e78c889d5cc2c7554cce4a9b12992c
Prakxo <87568477+Prakxo@users.noreply.github.com>
2023-08-11 03:49:09 +0100

    zurumode OK (#46)
    * zurumode OK
    
    * forgor to rename zuruKeyCheck
    
    * should remove warnings
    
    * refactor
    
    * rename pad to mgr
    
    * refactor 2

--- include/padmgr.h
+++ include/padmgr.h
@@ -3,6 +3,34 @@
 
 #include "ultra64.h"
 #include "libu64/pad.h"
+#include "unk.h"
+
+typedef struct Padmgr {
+    /* 0x000 */ u8 unk_000;
+    /* 0x004 */ void (*rumbleRetraceCallback)(void*);
+    /* 0x008 */ void* rumbleRetraceArg;
+    /* 0x00C */ void (*inputRetraceCallback)(void*);
+    /* 0x010 */ void* inputRetraceArg;
+    /* 0x014 */ UNK_TYPE1 unk_014[0x2B0 - 0x14];
+    /* 0x2B0 */ OSContPad pads[MAXCONTROLLERS];
+    /* 0x2C8 */ UNK_TYPE1 unk_2C8[0x480 - 0x2C8];
+} Padmgr; // size = 0x480
+
+#define PADMGR_SET_RETRACE_CALLACK(callback, arg) \
+    do {                                                  \
+        padmgr_class.inputRetraceCallback = (callback);           \
+        padmgr_class.inputRetraceArg = (arg);             \
+    } while (0)
+
+#define PADMGR_UNSET_RETRACE_CALLACK(callback, param) \
+do { \
+  Padmgr* mgr = &padmgr_class; \
+  if (mgr->inputRetraceCallback == (callback) && mgr->inputRetraceArg == (param)) { \
+        mgr->inputRetraceCallback = NULL; \
+        mgr->inputRetraceArg = NULL; \
+  } \
+} while (0)
+
 
 // void padmgr_LockSerialMesgQ();
 // void padmgr_UnlockSerialMesgQ();
@@ -25,5 +53,7 @@ void padmgr_ClearPadData(Input inputs[4]);
 // void func_800D763C_jp();
 // void func_800D765C_jp();
 // void func_800D7678_jp();
+
+extern Padmgr padmgr_class;
 
 #endif
