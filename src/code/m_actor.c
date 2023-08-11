87b0b1ecad8766e61ce0c80b23f3df862b6d57aa
angie <angheloalf95@gmail.com>
2023-08-10 21:02:24 -0400

    C file

---
+++ src/code/m_actor.c
@@ -0,0 +1,110 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/func_80056380_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/projection_pos_set.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_world_to_eye.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_position_move.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_position_speed_set.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_position_moveF.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_player_look_direction_check.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_display_position_set.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_data_bank_dma_end_check.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Shape_Info_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_foot_shadow_pos_set.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_delete.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_ct.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_dt.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_draw.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_draw_actor_no_culling_check.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_draw_actor_no_culling_check2.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_cull_check.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_delete_check.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_ct.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_dt.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_call_actor.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_draw_actor.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_part_new.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_part_delete.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_free_overlay_area.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/actor_free_check.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_get_overlay_area.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/func_80057940_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/func_80057A8C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/func_80057B70_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_data_bank_regist_check.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_malloc_actor_class.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_init_actor_class.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_make_actor.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_make_child_actor.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/restore_fgdata.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/restore_fgdata_one.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/restore_fgdata_all.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_save_actor.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_delete.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_name_search_sub.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_name_search.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_fgName_search_sub.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Actor_info_fgName_search.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Part_Break_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/HiliteReflect_new.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/HiliteReflect_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/HiliteReflect_xlu_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/HiliteReflect_light_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Setpos_HiliteReflect_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Setpos_HiliteReflect_xlu_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/Setpos_HiliteReflect_light_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_actor/RO_801161E8_jp.s")
