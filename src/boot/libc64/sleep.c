5f12959540beb8a8eda009a9773607dbebb6f292
Prakxo <lycos2003@gmail.com>
2023-08-15 20:11:46 +0200

    add array_count macro

--- src/boot/libc64/sleep.c
+++ src/boot/libc64/sleep.c
@@ -1,11 +1,12 @@
 #include "libc64/sleep.h"
+#include "macros.h"
 
 void csleep(OSTime t) {
     OSMesgQueue mq;
     OSMesg msg[1];
     OSTimer timer;
 
-    osCreateMesgQueue(&mq, msg, 1);
+    osCreateMesgQueue(&mq, msg, ARRAY_COUNT(msg));
     osSetTimer(&timer, t, 0, &mq, NULL);
     osRecvMesg(&mq, NULL, OS_MESG_BLOCK);
 }
