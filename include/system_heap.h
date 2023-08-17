ae5aa8db7c07a162cf140a30e9af772c57e4b16b
Derek Hensley <hensley.derek58@gmail.com>
2023-08-17 09:45:57 -0700

    system_heap (007060) OK (#60)
    * OK
    
    * Renames + cleanup
    
    * format
    
    ---------
    
    Co-authored-by: emilybrooks <emilybrooksemilybrooks@gmail.com>

---
+++ include/system_heap.h
@@ -0,0 +1,11 @@
+#ifndef SYSTEM_HEAP_H
+#define SYSTEM_HEAP_H
+
+#include "ultra64.h"
+#include "libc/stddef.h"
+
+void* SystemHeap_Malloc(size_t size);
+void SystemHeap_Free(void* ptr);
+void SystemHeap_Init(void* heap, size_t size);
+
+#endif
