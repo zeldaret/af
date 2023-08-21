ae70f5dd635c91a0678b293ea3f5c8c69bdf8e32
angie <angheloalf95@gmail.com>
2023-08-21 10:27:38 -0400

    C file

---
+++ src/code/m_submenu.c
@@ -0,0 +1,86 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C4420_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C4440_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C453C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C4648_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C46AC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/mSM_ovlptr_dllcnv.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/RO_STR_80117920_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/RO_STR_8011792C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C47B4_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C497C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C49D4_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/mSM_submenu_ovlptr_cleanup.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/load_player.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/mSM_submenu_ct.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/mSM_submenu_dt.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C4D8C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C4DB0_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C4DD8_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C4DFC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C4E2C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/mSM_submenu_ctrl.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C50C4_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C50EC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C510C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C5228_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C524C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/mSM_submenu_move.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/mSM_submenu_draw.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C53B8_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C543C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C54A8_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C54F4_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C557C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C55E4_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C5640_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C56AC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C5798_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C582C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C58B4_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C592C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C5960_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_submenu/func_800C5984_jp.s")
