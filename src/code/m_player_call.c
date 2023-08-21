41ece86f8c5ef2bd95c3a3662af1a4bbc494fa72
angie <angheloalf95@gmail.com>
2023-08-21 00:35:07 -0400

    C file

---
+++ src/code/m_player_call.c
@@ -0,0 +1,12 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_player_call/initfunc.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_player_call/Player_actor_ct_call.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_player_call/Player_actor_dt_call.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_player_call/Player_actor_move_call.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_player_call/Player_actor_draw_call.s")
