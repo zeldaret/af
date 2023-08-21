290004cffb77b4c30313e9058b1de6f2a583b5da
Derek Hensley <hensley.derek58@gmail.com>
2023-08-20 19:03:49 -0700

    math64 OK

--- include/libc64/fp.h
+++ include/libc64/fp.h
@@ -3,26 +3,30 @@
 
 #include "ultra64.h"
 
-// void floorf();
-// void floor();
-// void lfloorf();
-// void lfloor();
-// void ceilf();
-// void ceil();
-// void lceilf();
-// void lceil();
-// void truncf();
-// void trunc();
-// void ltruncf();
-// void ltrunc();
-// void nearbyintf();
+f32 floorf(f32 x);
+f64 floor(f64 x);
+s32 lfloorf(f32 x);
+s32 lfloor(f64 x);
+
+f32 ceilf(f32 x);
+f64 ceil(f64 x);
+s32 lceilf(f32 x);
+s32 lceil(f64 x);
+
+f32 truncf(f32 x);
+f64 trunc(f64 x);
+s32 ltruncf(f32 x);
+s32 ltrunc(f64 x);
+
+f32 nearbyintf(f32 x);
 f64 nearbyint(f64 x);
-// void lnearbyintf();
-// void lnearbyint();
-// void roundf();
-// void round();
-// void lroundf();
-// void lround();
+s32 lnearbyintf(f32 x);
+s32 lnearbyint(f64 x);
+
+f32 roundf(f32 x);
+f64 round(f64 x);
+s32 lroundf(f32 x);
+s32 lround(f64 x);
 
 extern f32 qNaN0x3FFFFF;
 extern f32 qNaN0x10000;
