0f32e98ddcb753975c9b390fc13751c86c4255b0
Derek Hensley <hensley.derek58@gmail.com>
2023-08-20 18:19:55 -0700

    syncprintf OK

---
+++ src/boot/syncprintf.c
@@ -0,0 +1,12 @@
+#include "ultra64.h"
+#include "libc64/aprintf.h"
+#include "libc/stdarg.h"
+
+void osSyncPrintfUnused(const char* fmt, ...) {
+}
+
+void osSyncPrintf(const char* fmt, ...) {
+}
+
+void rmonPrintf(const char* fmt, ...) {
+}
