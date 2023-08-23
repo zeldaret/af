40753c53cb9ab88ce7dd5dfa48cbc5bb8e10128b
angie <angheloalf95@gmail.com>
2023-08-22 21:07:55 -0400

    C file

---
+++ src/boot/z_std_dma.c
@@ -0,0 +1,58 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_800263F0_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/RO_STR_8003D1D0_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/RO_STR_8003D1D4_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/RO_STR_8003D1D8_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/RO_STR_8003D1EC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/RO_STR_8003D200_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/RO_STR_8003D214_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/RO_STR_8003D218_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/RO_STR_8003D22C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/DmaMgr_DmaRomToRam.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_800266C4_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026714_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026770_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_800267DC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026814_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/RO_8003D268_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026828_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_800269E4_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026A64_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/DmaMgr_RequestSync.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026BC0_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026C00_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026C28_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026C4C_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026CAC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026DA0_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026DCC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/boot/z_std_dma/func_80026E10_jp.s")
