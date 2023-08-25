b5bc6a0b434b26c8c80e9fba47224d1e3c717dda
angie <angheloalf95@gmail.com>
2023-08-25 11:08:30 -0400

    func_8085D358_jp, func_8085D500_jp, func_8085D674_jp

--- include/m_submenu.h
+++ include/m_submenu.h
@@ -40,24 +40,51 @@ typedef enum InventoryItemList {
     /* 16 */ INVENTORY_ITEM_LIST_MAX
 } InventoryItemList;
 
-typedef void (*struct_8085E9B0_unk_10670)(struct mSM*);
-typedef void (*struct_8085E9B0_unk_10674)(struct mSM*, struct Game_Play*);
+typedef void (*struct_8085E9B0_unk_10670_unk_00)(struct mSM*);
+typedef void (*struct_8085E9B0_unk_10670_unk_04)(struct mSM*, struct Game_Play*);
 
+typedef struct struct_8085E9B0_unk_10088 {
+    /* 0x00 */ s32 unk_00;
+    /* 0x04 */ UNK_TYPE1 unk_04[0x4];
+    /* 0x08 */ UNK_TYPE4 unk_08;
+    /* 0x0C */ struct_8085E9B0_unk_10670_unk_00 unk_0C;
+    /* 0x10 */ struct_8085E9B0_unk_10670_unk_04 unk_10;
+    /* 0x14 */ UNK_TYPE4 unk_14;
+    /* 0x18 */ f32 unk_18;
+    /* 0x1C */ f32 unk_1C;
+    /* 0x20 */ f32 unk_20;
+    /* 0x24 */ f32 unk_24;
+    /* 0x28 */ UNK_TYPE1 unk_28[0x10];
+    /* 0x38 */ UNK_TYPE4 unk_38;
+    /* 0x3C */ UNK_TYPE4 unk_3C;
+    /* 0x40 */ UNK_TYPE4 unk_40;
+    /* 0x44 */ UNK_TYPE4 unk_44;
+} struct_8085E9B0_unk_10088; // size = 0x48
+
+typedef struct struct_8085E9B0_unk_10670 {
+    /* 0x00 */ struct_8085E9B0_unk_10670_unk_00 unk_00;
+    /* 0x04 */ struct_8085E9B0_unk_10670_unk_04 unk_04;
+    /* 0x08 */ UNK_FUN_PTR unk_08;
+    /* 0x0C */ UNK_FUN_PTR unk_0C;
+    /* 0x10 */ UNK_FUN_PTR unk_10;
+    /* 0x14 */ UNK_FUN_PTR unk_14;
+    /* 0x18 */ UNK_TYPE1 unk_18[0x4];
+    /* 0x1C */ UNK_TYPE4 unk_1C;
+    /* 0x20 */ UNK_TYPE4 unk_20;
+    /* 0x24 */ s16 unk_24;
+    /* 0x26 */ u16 unk_26;
+    /* 0x28 */ f32 unk_28;
+    /* 0x2C */ f32 unk_2C;
+} struct_8085E9B0_unk_10670; // size >= 0x30
 
 typedef struct struct_8085E9B0 {
     /* 0x00000 */ UNK_TYPE1 unk_00000[0x10000];
     /* 0x10000 */ UNK_TYPE4 unk_10000;
-    /* 0x10004 */ UNK_TYPE1 unk_10004[0x66C];
+    /* 0x10004 */ UNK_TYPE1 unk_10004[0x84];
+    /* 0x10088 */ struct_8085E9B0_unk_10088 unk_10088[UNK_SIZE];
+    /* 0x100D0 */ UNK_TYPE1 unk_100D0[0x5A0];
     /* 0x10670 */ struct_8085E9B0_unk_10670 unk_10670;
-    /* 0x10674 */ struct_8085E9B0_unk_10674 unk_10674;
-    /* 0x10678 */ UNK_FUN_PTR unk_10678;
-    /* 0x1067C */ UNK_FUN_PTR unk_1067C;
-    /* 0x10680 */ UNK_FUN_PTR unk_10680;
-    /* 0x10684 */ UNK_FUN_PTR unk_10684;
-    /* 0x10688 */ UNK_TYPE1 unk_10688[0xC];
-    /* 0x10694 */ UNK_TYPE1 unk_10694[0x2];
-    /* 0x10696 */ u16 unk_10696;
-    /* 0x10698 */ UNK_TYPE1 unk_10698[0xC];
+    /* 0x106A0 */ UNK_TYPE1 unk_106A0[0x4];
     /* 0x106A4 */ UNK_FUN_PTR unk_106A4;
     /* 0x106A8 */ UNK_FUN_PTR unk_106A8;
     /* 0x106AC */ UNK_FUN_PTR unk_106AC;
--- src/overlays/submenu/submenu_ovl/m_submenu_ovl.c
+++ src/overlays/submenu/submenu_ovl/m_submenu_ovl.c
@@ -2,9 +2,18 @@
 #include "m_submenu.h"
 #include "m_lib.h"
 #include "game.h"
+#include "sys_matrix.h"
 #include "overlays/gamestates/ovl_play/m_play.h"
 
 extern struct_8085E9B0 ovl_base;
+
+typedef struct struct_8085E4D0 {
+    /* 0x00 */ UNK_TYPE1 unk_00[0x20];
+} struct_8085E4D0; // size = 0x20
+extern struct_8085E4D0 D_8085E4D0_jp[];
+
+extern s32 D_8085E938_jp[];
+extern f32 D_FLT_8085E7E8_jp[][4];
 
 void func_8085BAC0_jp(mSM* arg0, GraphicsContext* gfxCtx, s32 arg1);
 
@@ -24,6 +33,7 @@ void func_8085BAC0_jp(mSM* arg0, GraphicsContext* gfxCtx, s32 arg1);
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/submenu/submenu_ovl/m_submenu_ovl/func_8085D094_jp.s")
 
+void func_8085D128_jp(mSM* arg0, struct_8085E4D0* arg1);
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/submenu/submenu_ovl/m_submenu_ovl/func_8085D128_jp.s")
 
 void func_8085D244_jp(mSM* arg0);
@@ -31,21 +41,81 @@ void func_8085D244_jp(mSM* arg0);
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/submenu/submenu_ovl/m_submenu_ovl/func_8085D2EC_jp.s")
 
-void func_8085D358_jp(mSM* arg0);
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/submenu/submenu_ovl/m_submenu_ovl/func_8085D358_jp.s")
+void func_8085D358_jp(mSM* arg0) {
+    s32 temp_v0;
 
-void func_8085D398_jp(mSM* arg0);
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/submenu/submenu_ovl/m_submenu_ovl/func_8085D398_jp.s")
+    temp_v0 = arg0->unk_04;
+    func_8085D128_jp(arg0, &D_8085E4D0_jp[temp_v0]);
+    arg0->unk_08 = temp_v0;
+}
+
+void func_8085D398_jp(mSM* arg0) {
+    s32 temp_v0 = arg0->unk_04;
+    struct_8085E9B0_unk_10088* temp_v1 = &arg0->unk_2C->unk_10088[temp_v0];
+    f32* temp_a1 = D_FLT_8085E7E8_jp[temp_v0];
+
+    temp_v1->unk_00 = temp_v0;
+    temp_v1->unk_18 = temp_a1[0];
+    temp_v1->unk_1C = temp_a1[1];
+    temp_v1->unk_20 = temp_a1[2];
+    temp_v1->unk_24 = temp_a1[3];
+    temp_v1->unk_38 = arg0->unk_10;
+    temp_v1->unk_3C = arg0->unk_14;
+    temp_v1->unk_40 = arg0->unk_18;
+    temp_v1->unk_44 = arg0->unk_1C;
+
+    if ((temp_v0 == 1) && (arg0->unk_10 == 0xE)) {
+        temp_v1->unk_18 = -300.0f;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/submenu/submenu_ovl/m_submenu_ovl/func_8085D43C_jp.s")
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/submenu/submenu_ovl/m_submenu_ovl/func_8085D4D4_jp.s")
 
-void func_8085D500_jp(mSM* arg0);
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/submenu/submenu_ovl/m_submenu_ovl/func_8085D500_jp.s")
+void func_8085D500_jp(mSM* arg0) {
+    s32 var_a0 = (getButton() & 0xF) | getTrigger();
+    struct_8085E9B0_unk_10670* temp_v1 = &arg0->unk_2C->unk_10670;
 
+    if (gamePT->controller.moveR > 0.5f) {
+        u16 temp = gamePT->controller.moveAngle + 0x2000;
+
+        var_a0 |= D_8085E938_jp[temp >> 0xE];
+    }
+
+    if (var_a0 == temp_v1->unk_20) {
+        if (temp_v1->unk_24 > 0) {
+            temp_v1->unk_24--;
+            var_a0 = 0;
+        } else {
+            temp_v1->unk_24 = 1;
+        }
+    } else {
+        temp_v1->unk_20 = var_a0;
+        temp_v1->unk_24 = 0xC;
+    }
+
+    temp_v1->unk_1C = var_a0;
+}
+
+#if 0
+void func_8085D5D4_jp(mSM* arg0) {
+    s32 temp_a2;
+    struct_8085E9B0* temp_v0;
+    struct_8085E9B0_unk_10088* temp_v1;
+
+    temp_v1 = &arg0->unk_2C->unk_10088[arg0->unk_04];
+    temp_v0 = arg0->unk_2C;
+    temp_a2 = arg0->unk_08;
+    temp_v1->unk_08 = temp_a2;
+    temp_v1->unk_0C = temp_v0->unk_10670.unk_00;
+    temp_v1->unk_10 = temp_v0->unk_10670.unk_04;
+    temp_v0->unk_10088[temp_a2].unk_14 = arg0->unk_04;
+}
+#else
 void func_8085D5D4_jp(mSM* arg0);
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/submenu/submenu_ovl/m_submenu_ovl/func_8085D5D4_jp.s")
+#endif
 
 void func_8085D638_jp(mSM* arg0) {
     func_8085D5D4_jp(arg0);
@@ -54,28 +124,21 @@ void func_8085D638_jp(mSM* arg0) {
     func_8085D244_jp(arg0);
 }
 
-#if 0
 void func_8085D674_jp(mSM* arg0) {
-    void (**temp_v0)(mSM*);
+    struct_8085E9B0_unk_10670* temp_v0;
 
     temp_v0 = &arg0->unk_2C->unk_10670;
-    temp_v0->unk_28 = (f32) (temp_v0->unk_28 + 0.707f);
-    temp_v0->unk_2C = (f32) (temp_v0->unk_2C + 0.707f);
-    if (temp_v0->unk_28 >= 1024.0f) {
-        do {
-            temp_v0->unk_28 = (f32) (temp_v0->unk_28 - 1024.0f);
-        } while (temp_v0->unk_28 >= 1024.0f);
+    temp_v0->unk_28 += 0.707f;
+    temp_v0->unk_2C += 0.707f;
+
+    while (temp_v0->unk_28 >= 1024.0f) {
+        temp_v0->unk_28 -= 1024.0f;
     }
-    if (temp_v0->unk_2C >= 1024.0f) {
-        do {
-            temp_v0->unk_2C = (f32) (temp_v0->unk_2C - 1024.0f);
-        } while (temp_v0->unk_2C >= 1024.0f);
+
+    while (temp_v0->unk_2C >= 1024.0f) {
+        temp_v0->unk_2C -= 1024.0f;
     }
 }
-#else
-void func_8085D674_jp(mSM* arg0);
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/submenu/submenu_ovl/m_submenu_ovl/func_8085D674_jp.s")
-#endif
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/submenu/submenu_ovl/m_submenu_ovl/func_8085D718_jp.s")
 
@@ -93,12 +156,12 @@ void func_8085DA28_jp(mSM* arg0) {
         func_8085D638_jp(arg0);
     }
     func_8085D674_jp(arg0);
-    (*sp24)(arg0);
+    sp24->unk_00(arg0);
 }
 
 void func_8085DA9C_jp(mSM* arg0, Game_Play* game_play) {
     func_8085BAC0_jp(arg0, game_play->state.gfxCtx, 1);
-    arg0->unk_2C->unk_10674(arg0, game_play);
+    arg0->unk_2C->unk_10670.unk_04(arg0, game_play);
 }
 
 #if 0
