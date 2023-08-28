6858865cef789461618da8b5f19980da98008e64
Derek Hensley <hensley.derek58@gmail.com>
2023-08-27 19:47:44 -0700

    format

--- src/boot/libu64/mtxuty-cvt.c
+++ src/boot/libu64/mtxuty-cvt.c
@@ -4,12 +4,11 @@ void MtxConv_F2L(Mtx* mtx, MtxF* mf) {
     s32 i;
     s32 j;
 
-
     for (i = 0; i < 4; i++) {
         for (j = 0; j < 4; j++) {
             s32 value = (mf->mf[i][j] * 0x10000);
-            u16 (*intPart)[4] = (u16 (*)[4])mtx->m;
-            u16 (*fracPart)[4] = (u16 (*)[4])mtx->m[2];
+            u16(*intPart)[4] = (u16(*)[4])mtx->m;
+            u16(*fracPart)[4] = (u16(*)[4])mtx->m[2];
 
             intPart[i][j] = value >> 16;
             fracPart[i][j] = value;
