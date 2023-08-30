8674555938c62e5c1f9c6616c4106ba5631b5592
MegaIDK <pc@DESKTOP-37QIOUU.localdomain>
2023-08-30 12:57:42 -0300

    fun names

--- src/overlays/actors/ovl_Kamakura_Npc0/ac_kamakura_npc0.c
+++ src/overlays/actors/ovl_Kamakura_Npc0/ac_kamakura_npc0.c
@@ -8,11 +8,11 @@ void aKM0_actor_ct(Actor* thisx, Game_Play* game_play);
 void aKM0_actor_dt(Actor* thisx, Game_Play* game_play);
 void aKM0_actor_init(Actor* thisx, Game_Play* game_play);
 void aKM0_actor_save(Actor* thisx, Game_Play* game_play);
-void func_809DF884_jp(UNK_TYPE arg1, UNK_TYPE arg2);
-void func_809DF8B4_jp(UNK_TYPE arg1, UNK_TYPE arg2);
-s32 func_809DF8E4_jp(UNK_TYPE arg1, UNK_TYPE arg2);
-void func_809DF92C_jp(void);
-void func_809DF958_jp(void);
+void aKM0_talk_request(UNK_TYPE arg1, UNK_TYPE arg2);
+void aKM0_talk_init(UNK_TYPE arg1, UNK_TYPE arg2);
+s32 aKM0_talk_end_chk(UNK_TYPE arg1, UNK_TYPE arg2);
+void aKM0_actor_move(void);
+void aKM0_actor_draw(void);
 
 ActorProfile Kamakura_Npc0_Profile = {
     /* */ ACTOR_KAMAKURA_NPC0,
@@ -28,13 +28,13 @@ ActorProfile Kamakura_Npc0_Profile = {
     /* */ aKM0_actor_save,
 };
 
-struct_809AEFA4 D_809DF9B4_jp = {
-    func_809DF92C_jp, func_809DF958_jp, 3, func_809DF884_jp, func_809DF8B4_jp, func_809DF8E4_jp,
+struct_809AEFA4 aKM0_ct_data = {
+    aKM0_actor_move, aKM0_actor_draw, 3, aKM0_talk_request, aKM0_talk_init, aKM0_talk_end_chk,
 };
 
 void aKM0_actor_ct(Actor* thisx, Game_Play* game_play) {
     if (common_data.unk_1004C->unk_BC(thisx, game_play) == 1) {
-        common_data.unk_1004C->unk_C0(thisx, game_play, &D_809DF9B4_jp);
+        common_data.unk_1004C->unk_C0(thisx, game_play, &aKM0_ct_data);
         common_data.unk_1004C->unk_118(thisx);
     }
 }
@@ -51,15 +51,15 @@ void aKM0_actor_init(Actor* thisx, Game_Play* game_play) {
     common_data.unk_1004C->unk_CC(thisx, game_play);
 }
 
-void func_809DF884_jp(UNK_TYPE arg1, UNK_TYPE arg2 UNUSED) {
+void aKM0_talk_request(UNK_TYPE arg1, UNK_TYPE arg2 UNUSED) {
     common_data.unk_10078->unk_00(arg1);
 }
 
-void func_809DF8B4_jp(UNK_TYPE arg1, UNK_TYPE arg2 UNUSED) {
+void aKM0_talk_init(UNK_TYPE arg1, UNK_TYPE arg2 UNUSED) {
     common_data.unk_10078->unk_04(arg1);
 }
 
-s32 func_809DF8E4_jp(UNK_TYPE arg1, UNK_TYPE arg2 UNUSED) {
+s32 aKM0_talk_end_chk(UNK_TYPE arg1, UNK_TYPE arg2 UNUSED) {
     s32 var_v1;
     s32 sp1C;
 
@@ -71,10 +71,10 @@ s32 func_809DF8E4_jp(UNK_TYPE arg1, UNK_TYPE arg2 UNUSED) {
     return var_v1;
 }
 
-void func_809DF92C_jp(void) {
+void aKM0_actor_move(void) {
     common_data.unk_1004C->unk_D0();
 }
 
-void func_809DF958_jp(void) {
+void aKM0_actor_draw(void) {
     common_data.unk_1004C->unk_E4();
 }
