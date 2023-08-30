3b20621ad61b4da3a04622c687df44d697ab9797
Prakxo <lycos2003@gmail.com>
2023-08-30 16:18:55 +0200

    fixes

--- include/sys_ucode.h
+++ include/sys_ucode.h
@@ -9,11 +9,13 @@ extern s64 gspF3DZEX2_NoN_PosLight_fifoDataStart[];
 extern s64 gspS2DEX2_fifoTextStart[];
 extern s64 gspS2DEX2_fifoDataStart[];
 
-// void ucode_GetRspBootTextStart();
-// void ucode_GetRspBootTextSize();
-// void ucode_GetPolyTextStart();
-// void ucode_GetPolyDataStart();
-// void ucode_GetSpriteTextStart();
-// void ucode_GetSpriteDataStart();
+
+
+s64* ucode_GetRspBootTextStart(void);
+size_t ucode_GetRspBootTextSize(void);
+s64* ucode_GetPolyTextStart(void);
+s64* ucode_GetPolyDataStart(void);
+s64* ucode_GetSpriteTextStart(void);
+s64* ucode_GetSpireDataStart(void);
 
 #endif
--- src/code/sys_ucode.c
+++ src/code/sys_ucode.c
@@ -6,28 +6,26 @@ s64* poly_tbl[] = { gspF3DZEX2_NoN_PosLight_fifoTextStart, gspF3DZEX2_NoN_PosLig
 
 s64* sprite_tbl[] = { gspS2DEX2_fifoTextStart, gspS2DEX2_fifoDataStart };
 
-extern s8 rspbootTextStart;
-
-s64* ucode_GetRspBootTextStart(void) {
-    return &rspbootTextStart;
+s64* ucode_GetRspBootTextStart() {
+    return rspbootTextStart;
 }
 
-size_t ucode_GetRspBootTextSize(void) {
-    return (size_t)(&rspbootTextEnd - &rspbootTextStart);
+size_t ucode_GetRspBootTextSize() {
+    return ((uintptr_t)rspbootTextEnd - (uintptr_t)rspbootTextStart);
 }
 
-s64* ucode_GetPolyTextStart(void) {
+s64* ucode_GetPolyTextStart() {
     return poly_tbl[0];
 }
 
-s64* ucode_GetPolyDataStart(void) {
+s64* ucode_GetPolyDataStart() {
     return poly_tbl[1];
 }
 
-s64* ucode_GetSpriteTextStart(void) {
+s64* ucode_GetSpriteTextStart() {
     return sprite_tbl[0];
 }
 
-s64* ucode_GetSpriteDataStart(void) {
+s64* ucode_GetSpriteDataStart() {
     return sprite_tbl[1];
 }
