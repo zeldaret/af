8e2f50dca5c072e361e1874d253a8c90d68076ba
angie <angheloalf95@gmail.com>
2023-10-01 18:39:08 -0300

    fix

--- src/overlays/gamestates/ovl_first_game/sys_romcheck.c
+++ src/overlays/gamestates/ovl_first_game/sys_romcheck.c
@@ -1,11 +1,12 @@
 #include "first_game.h"
 #include "carthandle.h"
 #include "m_nmi_buf.h"
+#include "attributes.h"
 
 void sys_romcheck(void) {
     u32 sp24;
     u8 version;
-    s32 pad;
+    UNUSED s32 pad;
 
     // Read the last 4 bytes from the ROM header
     osEPiReadIo(carthandle, 0x3C, &sp24);
