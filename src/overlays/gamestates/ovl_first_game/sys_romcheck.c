83f82120f0ca99123dac651a3397ac2336e38213
angie <angheloalf95@gmail.com>
2023-10-01 18:20:44 -0300

    sys_romcheck NON_MATCHING

--- src/overlays/gamestates/ovl_first_game/sys_romcheck.c
+++ src/overlays/gamestates/ovl_first_game/sys_romcheck.c
@@ -1,23 +1,18 @@
 #include "first_game.h"
+#include "carthandle.h"
+#include "m_nmi_buf.h"
 
-#if 0
-typedef struct NmiBuffer {
-    /* 0x00 */ UNK_TYPE1 unk_00[0x3C];
-    /* 0x3C */ s32 unk_3C;
-} NmiBuffer; // size = 0x40
-
+#ifdef NON_MATCHING
 void sys_romcheck(void) {
     u32 sp24;
-    s32 pad;
     u8 temp;
+    s32 pad;
 
-    osEPiReadIo(gCartHandle, 0x3CU, &sp24);
+    osEPiReadIo(carthandle, 0x3C, &sp24);
 
     temp = sp24;
     if (temp == 0x23) {
-
-        //osAppNMIBuffer->unk_3C = (s32) (osAppNMIBuffer->unk_3C | 8);
-        osAppNMIBuffer[0xF] |= 0x8;
+        APPNMI_ZURUMODE2_SET();
     }
 }
 #else
