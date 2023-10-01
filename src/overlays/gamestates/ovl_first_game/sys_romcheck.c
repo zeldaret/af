84e1bdc41ab8fa9da1e88874517873a6c4d3f51c
angie <angheloalf95@gmail.com>
2023-10-01 18:28:13 -0300

    match sys_romcheck

--- src/overlays/gamestates/ovl_first_game/sys_romcheck.c
+++ src/overlays/gamestates/ovl_first_game/sys_romcheck.c
@@ -2,7 +2,6 @@
 #include "carthandle.h"
 #include "m_nmi_buf.h"
 
-#ifdef NON_MATCHING
 void sys_romcheck(void) {
     u32 sp24;
     u8 temp;
@@ -11,10 +10,8 @@ void sys_romcheck(void) {
     osEPiReadIo(carthandle, 0x3C, &sp24);
 
     temp = sp24;
-    if (temp == 0x23) {
+    sp24 = 0x23;
+    if (temp == sp24) {
         APPNMI_ZURUMODE2_SET();
     }
 }
-#else
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/gamestates/ovl_first_game/sys_romcheck/sys_romcheck.s")
-#endif
