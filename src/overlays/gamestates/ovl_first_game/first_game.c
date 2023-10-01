bdd303cd50992ce5deb81566ef26c60c3c21d6b4
angie <angheloalf95@gmail.com>
2023-10-01 17:56:56 -0300

    update

--- src/overlays/gamestates/ovl_first_game/first_game.c
+++ src/overlays/gamestates/ovl_first_game/first_game.c
@@ -2,26 +2,25 @@
 #include "m_common_data.h"
 #include "67E840.h"
 #include "attributes.h"
-#include "boot_variables.h"
 #include "overlays/gamestates/ovl_second_game/second_game.h"
 
-void func_808029D0_jp(FirstGameState* this);
+void func_808029D0_jp(Game_FirstGame* this);
 void func_80802A60_jp(void);
 
-void func_808029D0_jp(FirstGameState* this) {
+void func_808029D0_jp(Game_FirstGame* this) {
     func_80802A60_jp();
     mBGM_ct();
     common_data_init();
 
     STOP_GAMESTATE(&this->state);
-    SET_NEXT_GAMESTATE(&this->state, second_game_init, sizeof(SecondGameState));
+    SET_NEXT_GAMESTATE(&this->state, second_game_init, sizeof(Game_SecondGame));
 }
 
-void first_game_cleanup(GameState* thisx UNUSED) {
+void first_game_cleanup(Game* thisx UNUSED) {
 }
 
-void first_game_init(GameState* thisx) {
-    FirstGameState* this = (FirstGameState*)thisx;
+void first_game_init(Game* thisx) {
+    Game_FirstGame* this = (Game_FirstGame*)thisx;
 
     thisx->destroy = first_game_cleanup;
     func_808029D0_jp(this);
