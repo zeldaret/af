641847095af607c8d9519c838b256b528859abb1
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-01-08 14:47:27 -0700

    tol_umb_01

---
+++ src/objects/tol_umb_01/tol_umb_01.c
@@ -0,0 +1,8 @@
+#include "tol_umb_01.h"
+
+#include "assets/jp/objects/tol_umb_01/tol_umb_01_v.vtx.inc.c"
+#include "assets/jp/objects/tol_umb_01/kasa_umb01_model.gfx.inc.c"
+#include "assets/jp/objects/tol_umb_01/e_umb01_model.gfx.inc.c"
+#include "assets/jp/objects/tol_umb_01/tol_umb_01_pal.palette.inc.c"
+#include "assets/jp/objects/tol_umb_01/tol_umb_01_kasa_tex_txt.ci4.inc.c"
+#include "assets/jp/objects/tol_umb_01/tol_umb_01_tuka_tex_txt.ci4.inc.c"
