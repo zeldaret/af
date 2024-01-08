037d72aea083bf4020b3ea9b03902c7d62d910fe
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-01-08 15:29:11 -0700

    umbrella 3

---
+++ src/objects/tol_umb_02/tol_umb_02.c
@@ -0,0 +1,9 @@
+#include "tol_umb_02.h"
+
+#include "assets/jp/objects/tol_umb_02/tol_umb_02_v.vtx.inc.c"
+#include "assets/jp/objects/tol_umb_02/kasa_umb02_model.gfx.inc.c"
+#include "assets/jp/objects/tol_umb_02/e_umb02_model.gfx.inc.c"
+#include "assets/jp/objects/tol_umb_02/tol_umb02_pal.palette.inc.c"
+#include "assets/jp/objects/tol_umb_02/tol_umb02_kasa2_tex_txt.ci4.inc.c"
+#include "assets/jp/objects/tol_umb_02/tol_umb02_kasa1_tex_txt.ci4.inc.c"
+#include "assets/jp/objects/tol_umb_02/tol_umb02_tuka_tex_txt.ci4.inc.c"
---
+++ src/objects/tol_umb_03/tol_umb_03.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_TOL_UMB_03_H
+#define OBJECT_TOL_UMB_03_H
+
+#include "gbi.h"
+
+extern Vtx tol_umb_03_v[];
+extern Gfx kasa_umb03_model[];
+extern Gfx e_umb03_model[];
+extern unsigned short tol_umb_03_pal[];
+extern unsigned char tol_umb_03_kasa1_tex_txt[];
+extern unsigned char tol_umb_03_tuka_tex_txt[];
+
+#endif
