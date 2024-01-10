b5cfc635c2885cb0a3b550c1ab7f5d0270cb3387
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-01-10 10:20:19 -0700

    17

---
+++ src/objects/tol_umb_17/tol_umb_17.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_TOL_UMB_17_H
+#define OBJECT_TOL_UMB_17_H
+
+#include "gbi.h"
+
+extern Vtx tol_umb_17_v[];
+extern Gfx kasa_umb17_model[];
+extern Gfx e_umb17_model[];
+extern unsigned short tol_umb_17_pal[];
+extern unsigned char tol_umb_17_kasa_tex_txt[];
+extern unsigned char tol_umb_17_tuka_tex_txt[];
+
+#endif
