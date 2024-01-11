52db93e032f61360048f1385740986b545f4a3eb
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-01-11 16:56:02 -0700

    22

---
+++ src/objects/tol_umb_18/tol_umb_18.h
@@ -0,0 +1,14 @@
+#ifndef OBJECT_TOL_UMB_18_H
+#define OBJECT_TOL_UMB_18_H
+
+#include "gbi.h"
+
+extern Vtx tol_umb_18_v[];
+extern Gfx kasa_umb18_model[];
+extern Gfx e_umb18_model[];
+extern unsigned short tol_umb18_pal[];
+extern unsigned char tol_umb18_kasa1_tex_txt[];
+extern unsigned char tol_umb18_kasa2_tex_txt[];
+extern unsigned char tol_umb18_tuka_tex_txt[];
+
+#endif
---
+++ src/objects/tol_umb_22/tol_umb_22.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_TOL_UMB_22_H
+#define OBJECT_TOL_UMB_22_H
+
+#include "gbi.h"
+
+extern Vtx tol_umb_22_v[];
+extern Gfx kasa_umb22_model[];
+extern Gfx e_umb22_model[];
+extern unsigned short tol_umb_22_pal[];
+extern unsigned char tol_umb_22_kasa_tex_txt[];
+extern unsigned char tol_umb_22_e_tex_txt[];
+
+#endif
