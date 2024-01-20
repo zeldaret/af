3e2bc76b2b86511ad12ac2f6c95c8ee26836fe86
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-01-20 13:55:43 -0700

    tol_keitai_1

---
+++ src/objects/tol_keitai_1/tol_keitai_1.h
@@ -0,0 +1,28 @@
+#ifndef OBJECT_TOL_KEITAI_1_H
+#define OBJECT_TOL_KEITAI_1_H
+
+#include "gbi.h"
+#include "c_keyframe.h"
+
+extern u8 cKF_ckcb_r_tol_keitai_1_keitai_off1_tbl[];
+extern s16 cKF_kn_tol_keitai_1_keitai_off1_tbl[];
+extern s16 cKF_c_tol_keitai_1_keitai_off1_tbl[];
+extern Keyframe cKF_ds_tol_keitai_1_keitai_off1_tbl[];
+extern BaseAnimationR cKF_ba_r_tol_keitai_1_keitai_off1;
+extern u8 cKF_ckcb_r_tol_keitai_1_keitai_on1_tbl[];
+extern s16 cKF_kn_tol_keitai_1_keitai_on1_tbl[];
+extern s16 cKF_c_tol_keitai_1_keitai_on1_tbl[];
+extern Keyframe cKF_ds_tol_keitai_1_keitai_on1_tbl[];
+extern BaseAnimationR cKF_ba_r_tol_keitai_1_keitai_on1;
+extern Vtx tol_keitai_1_v[];
+extern Gfx main1_keitai1_model[];
+extern Gfx main2_keitai1_model[];
+extern unsigned short tol_keitai_1_pal[];
+extern unsigned char tol_keitai_1_ueura1_tex_txt[];
+extern unsigned char tol_keitai_1_ueomote1_tex_txt[];
+extern unsigned char tol_keitai_1_shitaura1_tex_txt[];
+extern unsigned char tol_keitai_1_shitaomote1_tex_txt[];
+extern JointElemR cKF_je_r_tol_keitai_1_tbl[];
+extern BaseSkeletonR cKF_bs_r_tol_keitai_1;
+
+#endif
