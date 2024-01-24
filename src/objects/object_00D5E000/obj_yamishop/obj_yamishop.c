f5e1e6ffedb7af0902575619575e8acc35850d89
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-01-24 15:35:03 -0700

    linked uranai

--- src/objects/object_00D5E000/obj_yamishop/obj_yamishop.c
+++ src/objects/object_00D5E000/obj_yamishop/obj_yamishop.c
@@ -1,6 +1,6 @@
 #include "obj_yamishop.h"
 
-s32 pad1[] = { 0x00000000, 0x00000000 };
+s32 padD5E000[] = { 0x00000000, 0x00000000 };
 #include "assets/jp/objects/object_00D5E000/obj_yamishop/obj_s_yamishop_v.vtx.inc.c"
 #include "assets/jp/objects/object_00D5E000/obj_yamishop/obj_s_yamishop_window_model.gfx.inc.c"
 #include "assets/jp/objects/object_00D5E000/obj_yamishop/obj_s_yamishop_t1_model.gfx.inc.c"
@@ -17,7 +17,7 @@ s32 pad1[] = { 0x00000000, 0x00000000 };
 #include "assets/jp/objects/object_00D5E000/obj_yamishop/cKF_c_obj_s_yamishop_tbl.inc.c"
 #include "assets/jp/objects/object_00D5E000/obj_yamishop/cKF_ds_obj_s_yamishop_tbl.inc.c"
 #include "assets/jp/objects/object_00D5E000/obj_yamishop/cKF_ba_r_obj_s_yamishop.inc.c"
-s32 pad2[] = { 0x00000000, 0x00000000, 0x00000000, 0x00000000 };
+s32 padD5FA18[] = { 0x00000000, 0x00000000, 0x00000000, 0x00000000 };
 #include "assets/jp/objects/object_00D5E000/obj_yamishop/obj_w_yamishop_v.vtx.inc.c"
 #include "assets/jp/objects/object_00D5E000/obj_yamishop/obj_w_yamishop_window_model.gfx.inc.c"
 #include "assets/jp/objects/object_00D5E000/obj_yamishop/obj_w_yamishop_t1_model.gfx.inc.c"
