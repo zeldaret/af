88fb5a372fbad6f4508080b60640a545cc3216aa
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-01-31 19:26:36 -0700

    obj_dump

---
+++ src/objects/object_00D5E000/obj_dump/obj_dump.h
@@ -0,0 +1,19 @@
+#ifndef OBJECT_OBJ_DUMP_H
+#define OBJECT_OBJ_DUMP_H
+
+#include "gbi.h"
+
+extern Vtx obj_s_dump_v[];
+extern Gfx obj_s_dump_t1T_model[];
+extern Gfx obj_s_dump_t2T_model[];
+extern Gfx dump_s_DL_model[];
+extern unsigned char obj_s_dump_t1_tex[];
+extern unsigned char obj_s_dump_t2_tex[];
+extern Vtx obj_w_dump_v[];
+extern Gfx obj_w_dump_t1T_model[];
+extern Gfx obj_w_dump_t2T_model[];
+extern Gfx dump_w_DL_model[];
+extern unsigned char obj_w_dump_t1_tex[];
+extern unsigned char obj_w_dump_t2_tex[];
+
+#endif
