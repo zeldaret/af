d15c0d56490a1cbcb9b2426ea3aa3b69c95c72a2
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-02-06 12:13:23 -0700

    fully matching

--- include/m_common_data.h
+++ include/m_common_data.h
@@ -24,6 +24,7 @@ struct CommonData_unk_1004C_unk_14_arg0;
 struct Game_Play;
 struct struct_809AEFA4;
 struct ToolClip;
+struct ShadowData;
 
 typedef UNK_RET (*CommonData_unk_1004C_unk_04)(struct ActorOverlay*, const struct struct_801161E8_jp*, size_t, s32);
 typedef UNK_RET (*CommonData_unk_1004C_unk_08)(void);
@@ -75,6 +76,14 @@ typedef struct CommonData_unk_10078 {
     /* 0x04 */ CommonData_unk_10078_unk_04 unk_04;
     /* 0x08 */ CommonData_unk_10078_unk_08 unk_08;
 } CommonData_unk_10078; // size >= 0xC
+
+typedef UNK_RET (*CommonData_unk_10080_unk_00)(UNK_TYPE);
+typedef void (*CommonData_unk_10080_unk_04)(struct Game_Play* game_play, struct ShadowData* shadowData, s16 unk2);
+
+typedef struct CommonData_unk_10080 {
+    /* 0x00 */ CommonData_unk_10080_unk_00 unk_00;
+    /* 0x04 */ CommonData_unk_10080_unk_04 unk_04;
+} CommonData_unk_10080; // size >= 0x8
 
 typedef UNK_RET (*CommonData_unk_10098_unk_4)(struct ActorOverlay*, size_t);
 typedef UNK_RET (*CommonData_unk_10098_unk_8)(void);
@@ -206,7 +215,9 @@ typedef struct CommonData {
     /* 0x1004C */ CommonData_unk_1004C* unk_1004C;
     /* 0x10050 */ UNK_TYPE1 unk_10050[0x28];
     /* 0x10078 */ CommonData_unk_10078* unk_10078;
-    /* 0x1007C */ UNK_TYPE1 unk_1007C[0x1C];
+    /* 0x1007C */ UNK_TYPE1 unk_1007C[0x4];
+    /* 0x10080 */ CommonData_unk_10080* unk_10080;
+    /* 0x10084 */ UNK_TYPE1 unk_10084[0x14];
     /* 0x10098 */ CommonData_unk_10098* unk_10098;
     /* 0x1009C */ UNK_TYPE1 unk_1009C[0x4];
     /* 0x100A0 */ struct ToolClip* toolClip;
--- src/overlays/actors/ovl_Koinobori/ac_koinobori.c
+++ src/overlays/actors/ovl_Koinobori/ac_koinobori.c
@@ -9,6 +9,7 @@
 #include "m_field_info.h"
 #include "audio.h"
 #include "overlays/actors/player_actor/m_player.h"
+#include "m_rcp.h"
 
 #define THIS ((Koinobori*)thisx)
 
@@ -16,8 +17,8 @@ void aKOI_actor_ct(Actor* thisx, Game_Play* game_play);
 void aKOI_actor_dt(Actor* thisx, Game_Play* game_play);
 void aKOI_actor_init(Actor* thisx, Game_Play* game_play);
 void aKOI_actor_draw(Actor* thisx, Game_Play* game_play);
-void func_80A9369C_jp(Koinobori *this, s32 processIndex);
-void func_80A936BC_jp(Koinobori *this, s32 processIndex);
+void aKOI_set_bgOffset(Koinobori *this, s32 processIndex);
+void aKOI_setup_action(Koinobori *this, s32 processIndex);
 
 #if 0
 ActorProfile Koinobori_Profile = {
@@ -35,68 +36,63 @@ ActorProfile Koinobori_Profile = {
 };
 #endif
 
-// #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Koinobori/ac_koinobori/aKOI_actor_ct.s")
 extern BaseSkeletonR D_601F22C;
 
-void aKOI_actor_ct(Actor *thisx, Game_Play *game_play)
+void aKOI_actor_ct(Actor *thisx, Game_Play *game_play UNUSED)
 {
     Koinobori* this = THIS;
 
     gSegments[6] = OS_K0_TO_PHYSICAL(common_data.unk_10098->unk_AC(0x27));
     cKF_SkeletonInfo_R_ct(&this->skeletonInfo, &D_601F22C, 0, this->jointTable, this->morphTable);
-    func_80A9369C_jp(this, 1);
-    func_80A936BC_jp(this, 0);
+    aKOI_set_bgOffset(this, 1);
+    aKOI_setup_action(this, 0);
     cKF_SkeletonInfo_R_play(&this->skeletonInfo);
 }
 
-// #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Koinobori/ac_koinobori/aKOI_actor_dt.s")
-void aKOI_actor_dt(Actor *thisx, Game_Play *game_play)
+void aKOI_actor_dt(Actor *thisx, Game_Play *game_play UNUSED)
 {
     Koinobori* this = THIS;
 
-    common_data.unk_10098->unk_A8(&common_data.unk_10098->unk_B0, 8, 0x27, this);
-    common_data.unk_10098->unk_A8(&common_data.unk_10098->unk_454, 9, 0x50, this);
-    common_data.unk_10098->unk_A8(&common_data.unk_10098->unk_86C, 8, 0x27, this);
+    common_data.unk_10098->unk_A8(&common_data.unk_10098->unk_B0, 8, 0x27, &this->actor);
+    common_data.unk_10098->unk_A8(&common_data.unk_10098->unk_454, 9, 0x50, &this->actor);
+    common_data.unk_10098->unk_A8(&common_data.unk_10098->unk_86C, 8, 0x27, &this->actor);
     cKF_SkeletonInfo_R_dt(&this->skeletonInfo);
 }
 
-// #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Koinobori/ac_koinobori/func_80A9369C_jp.s")
-void func_80A9369C_jp(Koinobori *this, s32 processIndex)
+void aKOI_set_bgOffset(Koinobori *this UNUSED, s32 processIndex UNUSED)
 {
 
 }
 
-// #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Koinobori/ac_koinobori/func_80A936AC_jp.s")
-void func_80A936AC_jp(Koinobori *this, s32 processIndex)
+void aKOI_wait(Koinobori *this UNUSED, s32 processIndex UNUSED)
 {
 
 }
 
-// #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Koinobori/ac_koinobori/func_80A936BC_jp.s")
 extern BaseAnimationR D_601F6D4;
 extern KoinoboriActionFunc D_80A93AB0_jp[];
 
-void func_80A936BC_jp(Koinobori *this, s32 processIndex)
+void aKOI_setup_action(Koinobori *this, s32 processIndex)
 {
     cKF_SkeletonInfo_R_init(&this->skeletonInfo, this->skeletonInfo.skeleton, &D_601F6D4, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, ANIMATION_REPEAT, NULL);
     this->unk2A0 = D_80A93AB0_jp[processIndex];
     this->unk2B4 = processIndex;
 }
 
-// #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Koinobori/ac_koinobori/func_80A9373C_jp.s")
-void func_80A9373C_jp(Koinobori* this, Game_Play* game_play)
+void aKOI_actor_move(Actor *thisx, Game_Play* game_play)
 {
-    s32 pad[2];
+    Koinobori* this = THIS;
+    s32 pad UNUSED;
     Player* player = get_player_actor_withoutCheck(game_play);
     s32 sp40;
     s32 sp3C;
     s32 sp38;
     s32 sp34;
-    s32 pad2;
+    s32 pad2 UNUSED;
 
     mFI_Wpos2BlockNum(&sp40, &sp3C, this->actor.world.pos);
     mFI_Wpos2BlockNum(&sp38, &sp34, player->actor.world.pos);
-    if ((mDemo_Check(1, player) == 0) && (mDemo_Check(5, player) == 0) && ((sp40 != sp38) || (sp3C != sp34)))
+    if ((mDemo_Check(1, &player->actor) == 0) && (mDemo_Check(5, &player->actor) == 0) && ((sp40 != sp38) || (sp3C != sp34)))
     {
         Actor_delete(&this->actor);
         return;
@@ -107,14 +103,59 @@ void func_80A9373C_jp(Koinobori* this, Game_Play* game_play)
     sAdo_OngenPos(this, 0x35, &this->actor.world.pos);
 }
 
-// #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Koinobori/ac_koinobori/aKOI_actor_init.s")
 void aKOI_actor_init(Actor *thisx, Game_Play *game_play)
 {
     Koinobori* this = THIS;
 
     mFI_SetFG_common(0xF100, this->actor.home.pos, 0);
-    func_80A9373C_jp(thisx, game_play);
-    this->actor.update = func_80A9373C_jp;
+    aKOI_actor_move(thisx, game_play);
+    this->actor.update = aKOI_actor_move;
 }
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Koinobori/ac_koinobori/aKOI_actor_draw.s")
+#define OPEN_POLY_OPA_DISPS()                 \
+    {                                         \
+        Gfx* __polyOpa = __gfxCtx->polyOpa.p; \
+        int __opa_opened = 0;                 \
+        while (0)
+
+#define CLOSE_POLY_OPA_DISPS()       \
+    __gfxCtx->polyOpa.p = __polyOpa; \
+    (void)__opa_opened;              \
+    }                                \
+    while (0)
+
+extern ShadowData D_80A93A9C_jp; // aKOI_shadow_data
+extern u8 D_80A93AB4_jp[]; // aKOI_obj_e_koinobori_a_pal
+extern u8 D_80A93AD4_jp[]; // obj_e_koinobori_b_pal
+
+void aKOI_actor_draw(Actor *thisx, Game_Play *game_play)
+{
+    GraphicsContext *gfxCtx = game_play->state.gfxCtx;
+    Koinobori* this = THIS;
+    Mtx *mtx;
+    s32 pad UNUSED;
+    s32 object;
+    s32 palette;
+
+    mtx = GRAPH_ALLOC_NO_ALIGN(gfxCtx, this->skeletonInfo.skeleton->unk01 * sizeof(Mtx));
+    if (mtx != NULL)
+    {
+        object = common_data.unk_10098->unk_AC(0x27);
+        palette = common_data.unk_10098->unk_450(0x50);
+
+        // _texture_z_light_fog_prim_npc
+        func_800BD5E8_jp(gfxCtx);
+        OPEN_DISPS(gfxCtx);
+        OPEN_POLY_OPA_DISPS();
+        gSPSegment(__polyOpa++, 0x08, palette);
+        gSPSegment(__polyOpa++, 0x09, Lib_SegmentedToVirtual(&D_80A93AD4_jp));
+        gSPSegment(__polyOpa++, 0x0A, Lib_SegmentedToVirtual(&D_80A93AB4_jp));
+        gSegments[6] = OS_K0_TO_PHYSICAL(object);
+        gSPSegment(__polyOpa++, 0x06, object);
+        CLOSE_POLY_OPA_DISPS();
+        CLOSE_DISPS(gfxCtx);
+        if (1){}
+        cKF_Si3_draw_R_SV(game_play, &this->skeletonInfo, mtx, NULL, NULL, this);
+        common_data.unk_10080->unk_04(game_play, &D_80A93A9C_jp, 0x27);
+    }
+}
