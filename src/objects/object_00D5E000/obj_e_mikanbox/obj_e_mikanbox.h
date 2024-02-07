eea5a353188c92e11ea3e82dbe08705c0f5453f0
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-02-07 12:27:58 -0700

    house1

--- src/objects/object_00D5E000/obj_e_mikanbox/obj_e_mikanbox.h
+++ src/objects/object_00D5E000/obj_e_mikanbox/obj_e_mikanbox.h
@@ -3,6 +3,13 @@
 
 #include "gbi.h"
 
+/**
+ * @file obj_e_mikanbox
+ * みかん Box of tangerines
+ * The box that K.K. Slider sits on in front of the train station. Does not actually have any tangerines in it. There's
+ * also the copyright message that is displayed after the credits that scroll during K.K. Slider's performance.
+ */
+
 extern Vtx obj_e_mikanbox_v[];
 extern Gfx obj_e_mikanbox_model[];
 extern unsigned char obj_e_mikanbox_tex[];
---
+++ src/objects/object_00D5E000/obj_e_mikuji/obj_e_mikuji.c
@@ -0,0 +1,11 @@
+#include "obj_e_mikuji.h"
+
+s32 padD7FEE0[] = { 0x00000000, 0x00000000 };
+#include "assets/jp/objects/object_00D5E000/obj_e_mikuji/obj_e_mikuji_v.vtx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_e_mikuji/obj_e_mikuji_t3_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_e_mikuji/obj_e_mikuji_t1T_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_e_mikuji/obj_e_mikuji_t2T_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_e_mikuji/mikuji_DL_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_e_mikuji/obj_e_mikuji_t1_tex.ci4.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_e_mikuji/obj_e_mikuji_t2_tex.ci4.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_e_mikuji/obj_e_mikuji_t3_tex.ci4.inc.c"
