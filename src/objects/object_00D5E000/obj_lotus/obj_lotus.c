bebacf3ba88c75c66f60ffc6f48371430607338e
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-02-08 08:43:50 -0700

    1/4th of the structure object extracted (#141)
    * linked yamishop
    
    * linked uranai
    
    * need to fix evw extensions
    
    * count01 linked
    
    * linked count02
    
    * linked douzou
    
    * obj_dump
    
    * extracted the pointer tables
    
    * obj_falls
    
    * obj_e_kago
    
    * koinobori
    
    * lotus
    
    * link koinobori
    
    * mikanbox
    
    * house1
    
    * cleanup
    
    * fix formatting
    
    * comment out c file that doesn't exist yet

---
+++ src/objects/object_00D5E000/obj_lotus/obj_lotus.c
@@ -0,0 +1,23 @@
+#include "obj_lotus.h"
+
+s32 padD7D6F0[] = { 0x00000000, 0x00000000 };
+#include "assets/jp/objects/object_00D5E000/obj_lotus/obj_s_lotus_v.vtx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/obj_s_lotus_flower1_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/obj_s_lotus_leafH_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/obj_s_lotus_leafG_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/obj_s_lotus_leafF_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/obj_s_lotus_leafE_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/obj_s_lotus_leafD_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/obj_s_lotus_leafC_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/obj_s_lotus_leafB_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/obj_s_lotus_leafA_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/obj_lotus_flower_tex_txt.ci4.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/obj_lotus_leaf_tex_txt.ci4.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/cKF_je_r_obj_s_lotus_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/cKF_bs_r_obj_s_lotus.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/cKF_ckcb_r_obj_s_lotus_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/cKF_kn_obj_s_lotus_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/cKF_c_obj_s_lotus_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/cKF_ds_obj_s_lotus_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_lotus/cKF_ba_r_obj_s_lotus.inc.c"
+s32 padD7EFD0[] = { 0x00000000, 0x00000000, 0x00000000, 0x00000000 };
---
+++ src/objects/object_00D5E000/obj_yamishop/obj_yamishop.h
@@ -0,0 +1,45 @@
+#ifndef OBJECT_OBJ_YAMISHOP_H
+#define OBJECT_OBJ_YAMISHOP_H
+
+#include "gbi.h"
+#include "c_keyframe.h"
+
+/**
+ * @file obj_yamishop
+ * 闇ショップ Crazy Redd's tent
+ */
+
+extern Vtx obj_s_yamishop_v[];
+extern Gfx obj_s_yamishop_window_model[];
+extern Gfx obj_s_yamishop_t1_model[];
+extern Gfx obj_s_yamishop_light_model[];
+extern Gfx obj_s_yamishop_door1_model[];
+extern Gfx obj_s_yamishop_door2_model[];
+extern unsigned char obj_s_yamishop_t2_tex_txt[];
+extern unsigned char obj_s_yamishop_t1_tex_txt[];
+extern unsigned char obj_s_yamishop_window_tex_txt[];
+extern JointElemR cKF_je_r_obj_s_yamishop_tbl[];
+extern BaseSkeletonR cKF_bs_r_obj_s_yamishop;
+extern u8 cKF_ckcb_r_obj_s_yamishop_tbl[];
+extern s16 cKF_kn_obj_s_yamishop_tbl[];
+extern s16 cKF_c_obj_s_yamishop_tbl[];
+extern Keyframe cKF_ds_obj_s_yamishop_tbl[];
+extern BaseAnimationR cKF_ba_r_obj_s_yamishop;
+extern Vtx obj_w_yamishop_v[];
+extern Gfx obj_w_yamishop_window_model[];
+extern Gfx obj_w_yamishop_t1_model[];
+extern Gfx obj_w_yamishop_light_model[];
+extern Gfx obj_w_yamishop_door1_model[];
+extern Gfx obj_w_yamishop_door2_model[];
+extern unsigned char obj_w_yamishop_t2_tex_txt[];
+extern unsigned char obj_w_yamishop_t1_tex_txt[];
+extern unsigned char obj_w_yamishop_window_tex_txt[];
+extern JointElemR cKF_je_r_obj_w_yamishop_tbl[];
+extern BaseSkeletonR cKF_bs_r_obj_w_yamishop;
+extern u8 cKF_ckcb_r_obj_w_yamishop_tbl[];
+extern s16 cKF_kn_obj_w_yamishop_tbl[];
+extern s16 cKF_c_obj_w_yamishop_tbl[];
+extern Keyframe cKF_ds_obj_w_yamishop_tbl[];
+extern BaseAnimationR cKF_ba_r_obj_w_yamishop;
+
+#endif
