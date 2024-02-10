a3f081a67e392966768cdd501996f59484d37505
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-02-10 11:29:45 -0700

    house2

--- src/objects/object_00D5E000/obj_house/obj_house.h
+++ src/objects/object_00D5E000/obj_house/obj_house.h
@@ -4,11 +4,6 @@
 #include "gbi.h"
 #include "c_keyframe.h"
 
-/**
- * @file obj_house
- * Villager houses
- */
-
 extern Vtx obj_s_house1_v[];
 extern Gfx obj_s_house1_window_model[];
 extern Gfx obj_s_house1_light_model[];
@@ -34,5 +29,30 @@ extern s16 cKF_kn_obj_s_house1_out_tbl[];
 extern s16 cKF_c_obj_s_house1_out_tbl[];
 extern Keyframe cKF_ds_obj_s_house1_out_tbl[];
 extern BaseAnimationR cKF_ba_r_obj_s_house1_out;
+extern Vtx obj_s_house2_v[];
+extern Gfx obj_s_house2_window_model[];
+extern Gfx obj_s_house2_t3_model[];
+extern Gfx obj_s_house2_t2_model[];
+extern Gfx obj_s_house2_t1_model[];
+extern Gfx obj_s_house2_name_model[];
+extern Gfx obj_s_house2_light_model[];
+extern Gfx obj_s_house2_door_model[];
+extern unsigned char obj_s_house2_t3_tex_txt[];
+extern unsigned char obj_s_house2_window_tex_txt[];
+extern unsigned char obj_s_house2_t2_tex_txt[];
+extern unsigned char obj_s_house2_t1_tex_txt[];
+extern unsigned char obj_s_house2_name_tex_txt[];
+extern JointElemR cKF_je_r_obj_s_house2_tbl[];
+extern BaseSkeletonR cKF_bs_r_obj_s_house2;
+extern u8 cKF_ckcb_r_obj_s_house2_tbl[];
+extern s16 cKF_kn_obj_s_house2_tbl[];
+extern s16 cKF_c_obj_s_house2_tbl[];
+extern Keyframe cKF_ds_obj_s_house2_tbl[];
+extern BaseAnimationR cKF_ba_r_obj_s_house2;
+extern u8 cKF_ckcb_r_obj_s_house2_out_tbl[];
+extern s16 cKF_kn_obj_s_house2_out_tbl[];
+extern s16 cKF_c_obj_s_house2_out_tbl[];
+extern Keyframe cKF_ds_obj_s_house2_out_tbl[];
+extern BaseAnimationR cKF_ba_r_obj_s_house2_out;
 
 #endif
