e8611edbf6f0be2f58cf8262fb11eca111077a5a
Derek Hensley <hensley.derek58@gmail.com>
2024-02-15 08:21:20 -0800

    Start prerender

---
+++ src/code/PreRender.c
@@ -0,0 +1,52 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/wallpaper_draw1.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/wallpaper_draw.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/gfx_SetUpCFB.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_setup_savebuf.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_setup_renderbuf.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_cleanup.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_TransBufferCopy.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_TransBuffer.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_TransBuffer1_env.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_TransBuffer1.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_TransBuffer2.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_ShowCoveredge.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_CopyRGBC.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_saveZBuffer.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_saveFrameBuffer.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_saveCVG.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_loadZBuffer.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_loadFrameBuffer.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_loadFrameBufferAlpha.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_loadFrameBufferCopy.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/ASAlgorithm.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/AntiAliasFilter.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_ConvertFrameBuffer_fg.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/PreRender/PreRender_ConvertFrameBuffer.s")
