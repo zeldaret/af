86ea7041561e43cf1d9940c8a109c3465e8b9a00
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-02-20 13:13:11 -0700

    fixed fall texture names

--- src/objects/object_00D5E000/obj_fallSE/obj_fallSE.c
+++ src/objects/object_00D5E000/obj_fallSE/obj_fallSE.c
@@ -7,19 +7,19 @@ s32 padD71A60[] = { 0x00000000, 0x00000000 };
 #include "assets/jp/objects/object_00D5E000/obj_fallSE/obj_fallSE_grpDT_model.gfx.inc.c"
 #include "assets/jp/objects/object_00D5E000/obj_fallSE/obj_fallSE_grpCT_model.gfx.inc.c"
 #include "assets/jp/objects/object_00D5E000/obj_fallSE/obj_fallSESW_model.gfx.inc.c"
-u8 obj_fallCA1_tex_rgb_ia8_2[] = {
+static u8 obj_fallCA1_tex_rgb_ia8[] = {
 #include "assets/jp/objects/object_00D5E000/obj_fallSE/obj_fallCA1_tex_rgb_ia8_2.ia8.inc.c"
 };
-u8 obj_fallA2_tex_rgb_i4_2[] = {
+static u8 obj_fallA2_tex_rgb_i4[] = {
 #include "assets/jp/objects/object_00D5E000/obj_fallSE/obj_fallA2_tex_rgb_i4_2.i4.inc.c"
 };
-u8 obj_fallA3_tex_rgb_i4_2[] = {
+static u8 obj_fallA3_tex_rgb_i4[] = {
 #include "assets/jp/objects/object_00D5E000/obj_fallSE/obj_fallA3_tex_rgb_i4_2.i4.inc.c"
 };
-u8 obj_fallC2_tex_rgb_i4_2[] = {
+static u8 obj_fallC2_tex_rgb_i4[] = {
 #include "assets/jp/objects/object_00D5E000/obj_fallSE/obj_fallC2_tex_rgb_i4_2.i4.inc.c"
 };
-u8 obj_fallC3_tex_rgb_i4_2[] = {
+static u8 obj_fallC3_tex_rgb_i4[] = {
 #include "assets/jp/objects/object_00D5E000/obj_fallSE/obj_fallC3_tex_rgb_i4_2.i4.inc.c"
 };
 #include "assets/jp/objects/object_00D5E000/obj_fallSE/obj_fallSE_evw_anime_1.inc.c"
