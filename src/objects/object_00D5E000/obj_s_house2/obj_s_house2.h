ebe722bc3fe08eb3af38e19635a8a99e40cce5ef
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-02-21 13:29:11 -0700

    split house

--- src/objects/object_00D5E000/obj_s_house2/obj_s_house2.h
+++ src/objects/object_00D5E000/obj_s_house2/obj_s_house2.h
@@ -1,34 +1,9 @@
-#ifndef OBJECT_OBJ_HOUSE_H
-#define OBJECT_OBJ_HOUSE_H
+#ifndef OBJECT_OBJ_S_HOUSE2_H
+#define OBJECT_OBJ_S_HOUSE2_H
 
 #include "gbi.h"
 #include "c_keyframe.h"
 
-extern Vtx obj_s_house1_v[];
-extern Gfx obj_s_house1_window_model[];
-extern Gfx obj_s_house1_light_model[];
-extern Gfx obj_s_house1_t4_model[];
-extern Gfx obj_s_house1_t3_model[];
-extern Gfx obj_s_house1_t2_model[];
-extern Gfx obj_s_house1_t1_model[];
-extern Gfx obj_s_house1_door_model[];
-extern u8 obj_s_house1_t1_tex_txt[];
-extern u8 obj_s_house1_window_txt[];
-extern u8 obj_s_house1_t3_tex_txt[];
-extern u8 obj_s_house1_t2_tex_txt[];
-extern u8 obj_s_house1_t4_tex_txt[];
-extern JointElemR cKF_je_r_obj_s_house1_tbl[];
-extern BaseSkeletonR cKF_bs_r_obj_s_house1;
-extern u8 cKF_ckcb_r_obj_s_house1_tbl[];
-extern s16 cKF_kn_obj_s_house1_tbl[];
-extern s16 cKF_c_obj_s_house1_tbl[];
-extern Keyframe cKF_ds_obj_s_house1_tbl[];
-extern BaseAnimationR cKF_ba_r_obj_s_house1;
-extern u8 cKF_ckcb_r_obj_s_house1_out_tbl[];
-extern s16 cKF_kn_obj_s_house1_out_tbl[];
-extern s16 cKF_c_obj_s_house1_out_tbl[];
-extern Keyframe cKF_ds_obj_s_house1_out_tbl[];
-extern BaseAnimationR cKF_ba_r_obj_s_house1_out;
 extern Vtx obj_s_house2_v[];
 extern Gfx obj_s_house2_window_model[];
 extern Gfx obj_s_house2_t3_model[];
