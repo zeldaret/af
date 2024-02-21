92382d7d00e499280b731a1fa2dfe3afd80b3a38
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-02-21 12:54:24 -0700

    split kago

---
+++ src/objects/object_00D5E000/obj_e_kago_w/obj_e_kago_w.h
@@ -0,0 +1,22 @@
+#ifndef OBJECT_OBJ_E_KAGO_W_H
+#define OBJECT_OBJ_E_KAGO_W_H
+
+#include "gbi.h"
+
+/**
+ * @file obj_e_kago_r
+ * 籠 Basket and balls for the Sports Fair ball toss (white)
+ */
+
+extern Vtx obj_e_kago_w_v[];
+extern Gfx obj_e_kago_w_t1T_model[];
+extern Gfx obj_e_kago_w_t2T_model[];
+extern Gfx kago_w_DL_model[];
+extern u8 obj_e_kago_w_t1_tex[];
+extern u8 obj_e_kago_w_t2_tex[];
+extern Vtx obj_e_kago_w_ball_v[];
+extern Gfx obj_e_kago_w_ball_t1T_model[];
+extern Gfx kago_w_ball_DL_model[];
+extern u8 obj_e_kago_w_ball_t1_tex[];
+
+#endif
