2ff6a88a855ba6485b79816f0087d8b030b83d6b
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-02-21 13:05:42 -0700

    split dump

---
+++ src/objects/object_00D5E000/obj_w_dump/obj_w_dump.h
@@ -0,0 +1,18 @@
+#ifndef OBJECT_OBJ_W_DUMP_H
+#define OBJECT_OBJ_W_DUMP_H
+
+#include "gbi.h"
+
+/**
+ * @file obj_dump
+ * The town dump (winter)
+ */
+
+extern Vtx obj_w_dump_v[];
+extern Gfx obj_w_dump_t1T_model[];
+extern Gfx obj_w_dump_t2T_model[];
+extern Gfx dump_w_DL_model[];
+extern u8 obj_w_dump_t1_tex[];
+extern u8 obj_w_dump_t2_tex[];
+
+#endif
