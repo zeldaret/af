dff72c4ef5cc0d081f7620f5bb445e1525675d00
Tom Overton <tom.overton@outlook.com>
2024-02-23 12:18:55 -0800

    ac_t_keitai OK

--- src/overlays/actors/ovl_T_Keitai/ac_t_keitai.h
+++ src/overlays/actors/ovl_T_Keitai/ac_t_keitai.h
@@ -2,17 +2,25 @@
 #define AC_T_KEITAI_H
 
 #include "ultra64.h"
+#include "c_keyframe.h"
 #include "m_actor.h"
+#include "overlays/actors/ovl_Tools/ac_tools.h"
 #include "unk.h"
 
 struct Game_Play;
 struct T_Keitai;
 
-typedef void (*T_KeitaiActionFunc)(struct T_Keitai*, struct Game_Play*);
+typedef void (*T_KeitaiActionFunc)(struct T_Keitai*);
 
 typedef struct T_Keitai {
-    /* 0x000 */ Actor actor;
-    /* 0x174 */ UNK_TYPE1 unk_174[0x100];
+    /* 0x000 */ ToolActor toolActor;
+    /* 0x1C0 */ UNK_TYPE1 unk_1C0[0x8];
+    /* 0x1C8 */ T_KeitaiActionFunc process;
+    /* 0x1CC */ s32 processIndex;
+    /* 0x1D0 */ SkeletonInfoR skeletonInfo;
+    /* 0x240 */ s_xyz jointTable[4];
+    /* 0x258 */ s_xyz morphTable[4];
+    /* 0x270 */ void* segment;
 } T_Keitai; // size = 0x274
 
 #endif
