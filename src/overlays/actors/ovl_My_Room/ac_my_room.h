8dee8010209d0a991ce401aaac009300bd74adeb
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-03-09 20:19:20 -0700

    fully matching

--- src/overlays/actors/ovl_My_Room/ac_my_room.h
+++ src/overlays/actors/ovl_My_Room/ac_my_room.h
@@ -8,6 +8,7 @@
 
 struct Game_Play;
 struct My_Room;
+struct Actor;
 
 
 typedef void (*My_RoomActionFunc)(struct My_Room*, struct Game_Play*);
@@ -22,7 +23,42 @@ typedef struct FurnitureActor {
     /* 0x134 */ SkeletonInfoR skeletonInfo;
     /* 0x1A4 */ s_xyz jointTable[9];
     /* 0x1DA */ s_xyz morphTable[9];
-    /* 0x210 */ Mtx matrix[2][12];
-} FurnitureActor; // size >= 0x210
+    /* 0x210 */ Mtx matrix[2][10]; // not sure if the array dimensions are correct
+} FurnitureActor; // size >= 0xA10
+
+typedef void (*FurnitureCreate)(FurnitureActor* this, u8* data);
+typedef void (*FurnitureMove)(FurnitureActor* this, struct Actor* myRoomActor, struct Game_Play* game_play, u8* data);
+typedef void (*FurnitureDraw)(FurnitureActor* this, struct Actor* myRoomActor, struct Game_Play* game_play, u8* data);
+typedef void (*FurnitureDestroy)(FurnitureActor* this, u8* data);
+typedef void (*FurnitureDma)(u16* name, u8* data);
+
+typedef struct FurnitureActorFunctions {
+    /* 0x00 */ FurnitureCreate furnitureCreate;
+    /* 0x04 */ FurnitureMove furnitureMove;
+    /* 0x08 */ FurnitureDraw furnitureDraw;
+    /* 0x0C */ FurnitureDestroy furnitureDestroy;
+    /* 0x10 */ FurnitureDma furnitureDma;
+} FurnitureActorFunctions; // size = 0x14
+
+typedef struct FurnitureActorUnkStruct {
+    /* 0x00 */ UNK_TYPE4 unk00;
+    /* 0x04 */ UNK_TYPE4 unk04;
+    /* 0x08 */ UNK_TYPE4 unk08;
+    /* 0x0C */ UNK_TYPE4 unk0C;
+    /* 0x10 */ UNK_TYPE4 unk10;
+    /* 0x14 */ UNK_TYPE4 unk14;
+    /* 0x18 */ UNK_TYPE4 unk18;
+    /* 0x1C */ UNK_TYPE4 unk1C;
+    /* 0x20 */ UNK_TYPE4 unk20;
+    /* 0x24 */ UNK_TYPE4 unk24;
+    /* 0x28 */ UNK_TYPE4 unk28;
+    /* 0x2C */ UNK_TYPE4 unk2C;
+    /* 0x30 */ UNK_TYPE4 unk30;
+    /* 0x34 */ UNK_TYPE4 unk34;
+    /* 0x38 */ UNK_TYPE4 unk38;
+    /* 0x3C */ UNK_TYPE4 unk3C;
+    /* 0x40 */ UNK_TYPE4 unk40;
+    /* 0x44 */ FurnitureActorFunctions* furnitureActorFunctions;
+} FurnitureActorUnkStruct; // size = 0x48
 
 #endif
--- src/overlays/furniture/ovl_sum_hal_chest02/ac_sum_hal_chest02.c
+++ src/overlays/furniture/ovl_sum_hal_chest02/ac_sum_hal_chest02.c
@@ -2,151 +2,92 @@
 #include "c_keyframe.h"
 #include "src/overlays/actors/ovl_My_Room/ac_my_room.h"
 
-// #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/furniture/ovl_sum_hal_chest02/ac_sum_hal_chest02/func_80A6FBE0_jp.s")
+void aSumHalChest02_ct(FurnitureActor* this, u8 data);
+void aSumHalChest02_mv(FurnitureActor* this, Actor* myRoomActor, Game_Play* game_play, u8* data);
+void aSumHalChest03_dw(FurnitureActor* this, Actor* myRoomActor, Game_Play* game_play, u8* data);
+void aSumHalChest03_dt(FurnitureActor* this, u8 data);
+
+FurnitureActorFunctions aSumHalChest02_func = {
+    /* */ aSumHalChest02_ct,
+    /* */ aSumHalChest02_mv,
+    /* */ aSumHalChest03_dw,
+    /* */ aSumHalChest03_dt,
+    /* */ NULL,
+};
+
+FurnitureActorUnkStruct iam_sum_hal_chest02 = {
+    /* */ 0x015C8000,
+    /* */ 0x015C9020,
+    /* */ 0x06000000,
+    /* */ 0x06001020,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x42200000,
+    /* */ 0x3C23D70A,
+    /* */ 0x04000000,
+    /* */ 0x00000004,
+    /* */ &aSumHalChest02_func,
+};
 
 extern BaseSkeletonR cKF_bs_r_int_sum_hal_chest02;
 extern BaseAnimationR cKF_ba_r_int_sum_hal_chest02;
 
-void func_80A6FBE0_jp(FurnitureActor* this, u8 data UNUSED) {
+void aSumHalChest02_ct(FurnitureActor* this, u8 data UNUSED) {
     UNUSED s32 pad[2];
     SkeletonInfoR* skeletonInfo = &this->skeletonInfo;
 
-    cKF_SkeletonInfo_R_ct(skeletonInfo, Lib_SegmentedToVirtual(&cKF_bs_r_int_sum_hal_chest02), Lib_SegmentedToVirtual(&cKF_ba_r_int_sum_hal_chest02), this->jointTable,
-                        this->morphTable);
+    cKF_SkeletonInfo_R_ct(skeletonInfo, Lib_SegmentedToVirtual(&cKF_bs_r_int_sum_hal_chest02),
+                          Lib_SegmentedToVirtual(&cKF_ba_r_int_sum_hal_chest02), this->jointTable, this->morphTable);
     cKF_SkeletonInfo_R_init_standard_stop(skeletonInfo, Lib_SegmentedToVirtual(&cKF_ba_r_int_sum_hal_chest02), NULL);
     skeletonInfo->frameControl.speed = 0.0f;
     cKF_SkeletonInfo_R_play(skeletonInfo);
 }
 
-// #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/furniture/ovl_sum_hal_chest02/ac_sum_hal_chest02/func_80A6FC74_jp.s")
-void func_80A6FC74_jp(FurnitureActor* this, Actor* myRoomActor, Game_Play* game_play, u8* data UNUSED)
-{
-    if (common_data.unk_1008C != NULL)
-    {
+void aSumHalChest02_mv(FurnitureActor* this, Actor* myRoomActor, Game_Play* game_play, u8* data UNUSED) {
+    if (common_data.unk_1008C != NULL) {
         common_data.unk_1008C->unk_34(this, myRoomActor, game_play, 1.0f, 16.0f);
     }
 }
 
-// #define OPEN_POLY_OPA_DISPS()                 \
-//     {                                         \
-//         Gfx* __polyOpa = __gfxCtx->polyOpa.p; \
-//         int __opa_opened = 0;                 \
-//         while (0)
+extern UNK_PTR D_6000020;
+extern UNK_PTR D_6000000;
 
-// #define CLOSE_POLY_OPA_DISPS()       \
-//     __gfxCtx->polyOpa.p = __polyOpa; \
-//     (void)__opa_opened;              \
-//     }                                \
-//     while (0)
+void aSumHalChest03_dw(FurnitureActor* this, Actor* myRoomActor UNUSED, Game_Play* game_play, u8* data UNUSED) {
+    UNUSED s32 pad;
+    s32 temp = game_play->state.unk_A0 & 1;
+    Mtx* mtx = &this->matrix[temp];
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/furniture/ovl_sum_hal_chest02/ac_sum_hal_chest02/func_80A6FCBC_jp.s")
-// void func_80A6FCBC_jp(FurnitureActor *this, Actor *myRoomActor, Game_Play *game_play, u8 *data)
-// {
-//     // s32 sp30;
-//     GraphicsContext *sp28;
-//     // void *sp20;
-//     // GraphicsContext *temp_a1;
-//     // s32 temp_t0;
-//     // void *temp_v0;
-//     // void *temp_v0_10;
-//     // void *temp_v0_11;
-//     // void *temp_v0_12;
-//     // void *temp_v0_13;
-//     // void *temp_v0_14;
-//     // void *temp_v0_2;
-//     // void *temp_v0_3;
-//     // void *temp_v0_4;
-//     // void *temp_v0_5;
-//     // void *temp_v0_6;
-//     // void *temp_v0_7;
-//     // void *temp_v0_8;
-//     // void *temp_v0_9;
-//     Mtx* mtx = this->matrix[game_play->state.unk_A0 & 1];
+    OPEN_DISPS(game_play->state.gfxCtx);
+    if (1) {}
+    if (1) {}
+    gSPMatrix(POLY_OPA_DISP++, _Matrix_to_Mtx_new(game_play->state.gfxCtx),
+              G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+    gDPSetTextureImage(POLY_OPA_DISP++, G_IM_FMT_I, G_IM_SIZ_16b, 1, &D_6000020);
+    gDPSetTile(POLY_OPA_DISP++, G_IM_FMT_I, G_IM_SIZ_16b, 0, 0x0000, G_TX_LOADTILE, 0, G_TX_MIRROR | G_TX_WRAP, 7,
+               G_TX_NOLOD, G_TX_MIRROR | G_TX_WRAP, 7, G_TX_NOLOD);
+    gDPLoadSync(POLY_OPA_DISP++);
+    gDPLoadBlock(POLY_OPA_DISP++, G_TX_LOADTILE, 0, 0, 2047, 0);
+    gDPPipeSync(POLY_OPA_DISP++);
+    gDPSetTile(POLY_OPA_DISP++, G_IM_FMT_I, G_IM_SIZ_8b, 8, 0x0000, G_TX_RENDERTILE, 0, G_TX_MIRROR | G_TX_WRAP, 7,
+               G_TX_NOLOD, G_TX_MIRROR | G_TX_WRAP, 7, G_TX_NOLOD);
+    gDPSetTileSize(POLY_OPA_DISP++, G_TX_RENDERTILE, 0, 0, 0x00FC, 0x00FC);
+    gDPSetTextureImage(POLY_OPA_DISP++, G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, &D_6000000);
+    gDPTileSync(POLY_OPA_DISP++);
+    gDPSetTile(POLY_OPA_DISP++, G_IM_FMT_RGBA, G_IM_SIZ_4b, 0, 0x01F0, G_TX_LOADTILE, 0, G_TX_NOMIRROR | G_TX_WRAP,
+               G_TX_NOMASK, G_TX_NOLOD, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMASK, G_TX_NOLOD);
+    gDPLoadSync(POLY_OPA_DISP++);
+    gDPLoadTLUTCmd(POLY_OPA_DISP++, G_TX_LOADTILE, 15);
+    gDPPipeSync(POLY_OPA_DISP++);
+    CLOSE_DISPS(game_play->state.gfxCtx);
 
-//     OPEN_DISPS(sp28);
-//     OPEN_POLY_OPA_DISPS();
-//     // temp_t0 = game_play->state.unk_A0 & 1;
-//     // temp_a1 = game_play->state.gfxCtx;
-//     // temp_v0 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0 + 8;
-//     // *temp_v0 = 0xDA380003;
-//     // sp30 = temp_t0;
-//     // sp28 = temp_a1;
-//     // sp20 = temp_v0;
-//     // sp20->unk4 = _Matrix_to_Mtx_new(game_play->state.gfxCtx);
-//     gSPMatrix(__polyOpa++, _Matrix_to_Mtx_new(game_play->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-//     // temp_v0_2 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_2 + 8;
-//     // temp_v0_2->unk0 = 0xFD900000;
-//     // temp_v0_2->unk4 = 0x06000020;
-//     gDPSetTextureImage(__polyOpa++, G_IM_FMT_I, G_IM_SIZ_16b, 1, 0x06000020);
-//     // temp_v0_3 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_3 + 8;
-//     // temp_v0_3->unk0 = 0xF5900000;
-//     // temp_v0_3->unk4 = (? *)0x0705C170;
-//     gDPSetTile(__polyOpa++, G_IM_FMT_I, G_IM_SIZ_16b, 0, 0x0000, G_TX_LOADTILE, 0, G_TX_MIRROR | G_TX_WRAP, 7, G_TX_NOLOD, G_TX_MIRROR | G_TX_WRAP, 7, G_TX_NOLOD);
-//     // temp_v0_4 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_4 + 8;
-//     // temp_v0_4->unk4 = NULL;
-//     // temp_v0_4->unk0 = 0xE6000000;
-//     gDPLoadSync(__polyOpa++);
-//     // temp_v0_5 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_5 + 8;
-//     // temp_v0_5->unk4 = (? *)0x077FF000;
-//     // temp_v0_5->unk0 = 0xF3000000;
-//     gDPLoadBlock(__polyOpa++, G_TX_LOADTILE, 0, 0, 2047, 0);
-//     // temp_v0_6 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_6 + 8;
-//     // temp_v0_6->unk4 = NULL;
-//     // temp_v0_6->unk0 = 0xE7000000;
-//     gDPPipeSync(__polyOpa++);
-//     // temp_v0_7 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_7 + 8;
-//     // temp_v0_7->unk4 = (? *)0x5C170;
-//     // temp_v0_7->unk0 = 0xF5881000;
-//     gDPSetTile(__polyOpa++, G_IM_FMT_I, G_IM_SIZ_8b, 8, 0x0000, G_TX_RENDERTILE, 0, G_TX_MIRROR | G_TX_WRAP, 7, G_TX_NOLOD, G_TX_MIRROR | G_TX_WRAP, 7, G_TX_NOLOD);
-//     // temp_v0_8 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_8 + 8;
-//     // temp_v0_8->unk0 = 0xF2000000;
-//     // temp_v0_8->unk4 = (? *)0xFC0FC;
-//     gDPSetTileSize(__polyOpa++, G_TX_RENDERTILE, 0, 0, 0x00FC, 0x00FC);
-//     // temp_v0_9 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_9 + 8;
-//     // temp_v0_9->unk0 = 0xFD100000;
-//     // temp_v0_9->unk4 = (? *)0x06000000;
-//     gDPSetTextureImage(__polyOpa++, G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, 0x06000000);
-//     // temp_v0_10 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_10 + 8;
-//     // temp_v0_10->unk4 = NULL;
-//     // temp_v0_10->unk0 = 0xE8000000;
-//     gDPTileSync(__polyOpa++);
-//     // temp_v0_11 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_11 + 8;
-//     // temp_v0_11->unk4 = (? *)0x07000000;
-//     // temp_v0_11->unk0 = 0xF50001F0;
-//     gDPSetTile(__polyOpa++, G_IM_FMT_RGBA, G_IM_SIZ_4b, 0, 0x01F0, G_TX_LOADTILE, 0, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMASK, G_TX_NOLOD);
-//     // temp_v0_12 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_12 + 8;
-//     // temp_v0_12->unk4 = NULL;
-//     // temp_v0_12->unk0 = 0xE6000000;
-//     gDPLoadSync(__polyOpa++);
-//     // temp_v0_13 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_13 + 8;
-//     // temp_v0_13->unk4 = (? *)0x0703C000;
-//     // temp_v0_13->unk0 = 0xF0000000;
-//     gDPLoadTLUTCmd(__polyOpa++, G_TX_LOADTILE, 15);
-//     // temp_v0_14 = temp_a1->polyOpa.tha.head;
-//     // temp_a1->polyOpa.tha.head = temp_v0_14 + 8;
-//     // temp_v0_14->unk4 = NULL;
-//     // temp_v0_14->unk0 = 0xE7000000;
-//     gDPPipeSync(__polyOpa++);
-//     CLOSE_POLY_OPA_DISPS();
-//     CLOSE_DISPS(gfxCtx);
+    cKF_Si3_draw_R_SV(game_play, &this->skeletonInfo, mtx, NULL, NULL, NULL);
+}
 
-//     cKF_Si3_draw_R_SV(game_play, &this->skeletonInfo, mtx, NULL, NULL, NULL);
-// }
-
-// #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/furniture/ovl_sum_hal_chest02/ac_sum_hal_chest02/func_80A6FED0_jp.s")
-void func_80A6FED0_jp(FurnitureActor* this UNUSED, u8 data UNUSED)
-{
+void aSumHalChest03_dt(FurnitureActor* this UNUSED, u8 data UNUSED) {
 }
