df325ad6c2a626c428271a6e25e1734415a3459d
Prakxo <lycos2003@gmail.com>
2024-04-02 01:06:38 +0200

    format/names

--- src/code/gfxalloc.c
+++ src/code/gfxalloc.c
@@ -1,29 +1,29 @@
 #include "gfxalloc.h"
 #include "alignment.h"
 
-Gfx* gfxopen(Gfx* gfx) {
-    return gfx + 1;
+Gfx* gfxopen(Gfx* gfxHead) {
+    return gfxHead + 1;
 }
 
-Gfx* gfxclose(Gfx* gfx, Gfx* gfxNew) {
-    if (gfx + 1 != gfxNew) {
-        gSPBranchList(gfx, gfxNew);
-        return gfxNew;
-    } else {
-        gSPNoOp(gfx);
+Gfx* gfxclose(Gfx* gfxHead, Gfx* gfx) {
+    if (gfxHead + 1 != gfx) {
+        gSPBranchList(gfxHead, gfx);
         return gfx;
+    } else {
+        gSPNoOp(gfxHead);
+        return gfxHead;
     }
 }
 
-void* gfxalloc(Gfx** gfxp, size_t size) {
+void* gfxalloc(Gfx** gfxP, size_t size) {
     u8* ptr;
     Gfx* dst;
 
     size = ALIGN8(size);
-    ptr = (u8*)(*gfxp + 1);
+    ptr = (u8*)(*gfxP + 1);
     dst = (Gfx*)(ptr + size);
-    gSPBranchList(*gfxp, dst);
+    gSPBranchList(*gfxP, dst);
 
-    *gfxp = dst;
+    *gfxP = dst;
     return ptr;
 }
