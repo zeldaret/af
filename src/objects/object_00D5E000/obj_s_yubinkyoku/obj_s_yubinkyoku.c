8e43aa7ec4ddb38b83e0e5ab8f744ee958577234
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-04-02 15:56:04 -0700

    s post office

---
+++ src/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku.c
@@ -0,0 +1,35 @@
+#include "obj_s_yubinkyoku.h"
+
+s32 padDADE50[] = { 0x00000000, 0x00000000 };
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku_v.vtx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku_window_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku_t3_1_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku_t2_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku_t1_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku_neon_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku_light_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku_door_model.gfx.inc.c"
+u8 obj_s_yubinkyoku_t3_txt[] = {
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku_t3_txt.ci4.inc.c"
+};
+u8 obj_s_yubinkyoku_t1_txt[] = {
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku_t1_txt.ci4.inc.c"
+};
+u8 obj_s_yubinkyoku_window_tex_txt[] = {
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku_window_tex_txt.i4.inc.c"
+};
+u8 obj_s_yubinkyoku_t2_txt[] = {
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/obj_s_yubinkyoku_t2_txt.ci4.inc.c"
+};
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/cKF_je_r_obj_s_yubinkyoku_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/cKF_bs_r_obj_s_yubinkyoku.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/cKF_ckcb_r_obj_s_yubinkyoku_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/cKF_kn_obj_s_yubinkyoku_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/cKF_c_obj_s_yubinkyoku_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/cKF_ds_obj_s_yubinkyoku_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/cKF_ba_r_obj_s_yubinkyoku.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/cKF_ckcb_r_obj_s_yubinkyoku_out_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/cKF_kn_obj_s_yubinkyoku_out_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/cKF_c_obj_s_yubinkyoku_out_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/cKF_ds_obj_s_yubinkyoku_out_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000/obj_s_yubinkyoku/cKF_ba_r_obj_s_yubinkyoku_out.inc.c"
