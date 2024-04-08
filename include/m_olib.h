e50f0c3a29869695a72fbf52eb0347dcf21800a9
shawlucas <lgs84@cornell.edu>
2024-04-08 03:10:00 -0400

    added functions to header file

--- include/m_olib.h
+++ include/m_olib.h
@@ -6,20 +6,20 @@
 #include "libc64/math64.h"
 #include "z64camera.h"
 
-// void distance_between();
-// void distance_between2();
-// void distance_2d();
-// void never_zero();
-// void limiter();
-// void unitvector_by_2pos();
-// void spolar2world();
-// void sglobe2world();
-// void world2spolar();
-// void world2sglobe();
-// void spolar_by_2pos();
-// void sglobe_by_2pos();
-// void radianxy_by_2pos();
-// void degreexy_by_2pos();
-// void sanglexy_by_2pos();
+f32 distance_between(xyz_t* a, xyz_t* b);
+f32 distance_between2(xyz_t* a, xyz_t* b, xyz_t* dest);
+f32 distance_2d(xyz_t* a, xyz_t* b);
+f32 never_zero(f32 val, f32 min);
+f32 limiter(f32 val, f32 max);
+xyz_t unitvector_by_2pos(xyz_t* a, xyz_t* b);
+xyz_t spolar2world(VecSph* sph);
+xyz_t sglobe2world(VecGeo* geo);
+VecSph world2spolar(xyz_t* vec);
+VecGeo world2sglobe(xyz_t* vec);
+VecSph spolar_by_2pos(xyz_t* a, xyz_t* b);
+VecGeo sglobe_by_2pos(xyz_t* a, xyz_t* b);
+xyz_t radianxy_by_2pos(xyz_t* a, xyz_t* b);
+xyz_t degreexy_by_2pos(xyz_t* a, xyz_t* b);
+s_xyz sanglexy_by_2pos(xyz_t* a, xyz_t* b);
 
 #endif
