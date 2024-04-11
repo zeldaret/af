3d0c75b8c0d5fcaba84418c567a863e8400f1e68
shawlucas <lgs84@cornell.edu>
2024-04-11 02:51:31 -0400

    m_debug_display.c OK

--- include/m_debug_display.h
+++ include/m_debug_display.h
@@ -2,13 +2,25 @@
 #define M_DEBUG_DISPLAY_H
 
 #include "ultra64.h"
+#include "overlays/gamestates/ovl_play/m_play.h"
+#include "z64math.h"
+#include "color.h"
 
 struct Game_Play;
 
+typedef struct DebugDispObject {
+    /* 0x00 */ xyz_t pos;
+    /* 0x0C */ s_xyz rot;
+    /* 0x14 */ xyz_t scale;
+    /* 0x20 */ Color_RGBA8 color;
+    /* 0x24 */ s16 type;
+    /* 0x28 */ struct DebugDispObject* next;
+} DebugDispObject; // size = 0x30
+
 void Debug_Display_init(void);
 // void Debug_Display_new();
-void Debug_Display_output(struct Game_Play* game_play);
-// void debug_display_output_sprite_16x16_I8();
+void Debug_Display_output(Game_Play* game_play);
+void debug_display_output_sprite_16x16_I8(DebugDispObject* dispObj, void* texture, Game_Play* game_play);
 // void debug_display_output_polygon();
 
 #endif
