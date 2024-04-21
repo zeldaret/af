a3d34dc3cca2f741c83b36f99d6fc23cbd7ff66c
Prakxo <lycos2003@gmail.com>
2024-04-21 12:48:14 +0200

    m_scene_ftr OK

--- src/code/m_scene_ftr.c
+++ src/code/m_scene_ftr.c
@@ -1,13 +1,13 @@
 #include "m_scene_ftr.h"
 #include "overlays/gamestates/ovl_play/m_play.h"
 
-void mScn_Dma2Bank(ObjectExchangeBank* exchange, s32 id, RomOffset vrom, size_t size){
+void mScn_Dma2Bank(ObjectExchangeBank* exchange, s32 id, RomOffset vrom, size_t size) {
 
-    if(vrom != NULL){
-        DmaMgr_RequestSyncDebug(exchange->status[id+3].vram, vrom, size, "../m_scene_ftr.c", 52);
+    if (vrom != NULL) {
+        DmaMgr_RequestSyncDebug(exchange->status[id + 3].unk14.vram, vrom, size, "../m_scene_ftr.c", 52);
     }
 }
- 
+
 void mScn_KeepMyRoomBankNew(ObjectExchangeBank* exchange, s16 id, size_t size) {
     ObjectStatus* bank = exchange->status + exchange->num;
 
@@ -22,7 +22,6 @@ void mScn_KeepMyRoomBankNew(ObjectExchangeBank* exchange, s16 id, size_t size) {
     }
 }
 
-
 void mScn_ObtainCarpetBank(Game_Play* play) {
     ObjectExchangeBank* exchange = &play->objectExchangeBank;
     s32 i;
@@ -36,10 +35,10 @@ void mScn_ObtainCarpetBank(Game_Play* play) {
     }
 }
 
-s32 mScn_ObtainFurnitureBank(UNUSED Game_Play* play, s32 bank){
+s32 mScn_ObtainFurnitureBank(UNUSED Game_Play* play, s32 bank) {
     common_data.clip.unk_020 = bank;
 }
 
-void mScn_ObtainMyRoomBank(Game_Play* play) { 
-    mScn_ObtainCarpetBank(play); 
+void mScn_ObtainMyRoomBank(Game_Play* play) {
+    mScn_ObtainCarpetBank(play);
 }
