48c8a77bbf4ec8ecac2642d5b39280583ef1355d
inspectredc <78732756+inspectredc@users.noreply.github.com>
2024-04-24 01:42:13 +0100

    ac_tama OK (#179)
    * ac_tama OK
    
    * or else

--- src/overlays/actors/ovl_Kago/ac_kago.c
+++ src/overlays/actors/ovl_Kago/ac_kago.c
@@ -42,7 +42,7 @@ void aKAG_actor_ct(Actor* thisx, UNUSED Game_Play* game_play) {
     this->type = this->actor.fgName - 0x5835;
     this->structureType = this->type + 0x23;
     this->structurePalette = this->type + 0x4C;
-    aKAG_setup_action(this, 0);
+    aKAG_setup_action(this, KAG_PROCESS_WAIT);
     aKAG_set_bgOffset(this, 1);
 }
 
