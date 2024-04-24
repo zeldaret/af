8caa1e847b1a08633bb2606e5d9e7ed5f6387885
inspectredc <78732756+inspectredc@users.noreply.github.com>
2024-04-24 16:43:28 +0100

    ac_dump OK (#180)
    * ac_dump OK
    
    * warnings

--- src/overlays/actors/ovl_Dump/ac_dump.h
+++ src/overlays/actors/ovl_Dump/ac_dump.h
@@ -8,11 +8,19 @@
 struct Game_Play;
 struct Dump;
 
+typedef enum DumpProcess {
+    DUM_PROCESS_WAIT,
+} DumpProcess;
+
 typedef void (*DumpActionFunc)(struct Dump*, struct Game_Play*);
 
 typedef struct Dump {
     /* 0x000 */ Actor actor;
-    /* 0x174 */ UNK_TYPE1 unk_174[0x164];
+    /* 0x174 */ UNK_TYPE1 unk_174[0x12C];
+    /* 0x2A0 */ DumpActionFunc process;
+    /* 0x2A4 */ UNK_TYPE1 unk_2A4[0x10];
+    /* 0x2B4 */ s32 processNum;
+    /* 0x2B8 */ UNK_TYPE1 unk_2B8[0x20];
 } Dump; // size = 0x2D8
 
 #endif
