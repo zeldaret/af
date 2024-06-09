248161b65b62502c71bee79fc4b905804c66df7b
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-06-09 14:58:58 -0700

    extracted locerrmsg

---
+++ src/locerrmsg/locerrmsg.c
@@ -0,0 +1,5 @@
+#include "locerrmsg.h"
+
+u8 not_designed_for_system_error[] = {
+#include "assets/jp/locerrmsg/not_designed_for_system_error.i4.inc.c"
+};
---
+++ src/locerrmsg/locerrmsg.h
@@ -0,0 +1,8 @@
+#ifndef OBJECT_LOCERRMSG_H
+#define OBJECT_LOCERRMSG_H
+
+#include "gbi.h"
+
+extern u8 not_designed_for_system_error[];
+
+#endif
