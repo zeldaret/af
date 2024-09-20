3fa5f228c8e32451a9ed1a472636c60346b0741d
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-09-20 11:36:27 -0700

    inported data

--- src/overlays/furniture/ovl_sum_clchest03/ac_sum_clchest03.c
+++ src/overlays/furniture/ovl_sum_clchest03/ac_sum_clchest03.c
@@ -2,41 +2,46 @@
 #include "c_keyframe.h"
 #include "src/overlays/actors/ovl_My_Room/ac_my_room.h"
 
+void aSumClChest03_ct(FurnitureActor* this, u8* data UNUSED);
+void aSumClChest03_mv(FurnitureActor *this, Actor *myroomactor, Game_Play *game_play, u8* data UNUSED);
+void aSumClChest03_dw(FurnitureActor* this, Actor* myRoomActor UNUSED, Game_Play* game_play, u8* data UNUSED);
+void aSumClChest03_dt(FurnitureActor* this UNUSED, u8* data UNUSED);
+
 // D_80A70F60_jp
-//  FurnitureActorFunctions aSumClChest03_func = {
-//      /* */ func_80A70C60_jp,
-//      /* */ func_80A70CF4_jp,
-//      /* */ func_8007244C_jp,
-//      /* */ func_80076358_jp,
-//      /* */ NULL,
-//  };
+FurnitureActorFunctions aSumClChest03_func = {
+    /* */ aSumClChest03_ct,
+    /* */ aSumClChest03_mv,
+    /* */ aSumClChest03_dw,
+    /* */ aSumClChest03_dt,
+    /* */ NULL,
+};
 
 // D_80A70F74_jp
-//  FurnitureActorUnkStruct iam_sum_clchest03 = {
-//      /* */ 0x015D0000,
-//      /* */ 0x015D0F70,
-//      /* */ 0x06000000,
-//      /* */ D_6000F70,
-//      /* */ 0x00000000,
-//      /* */ 0x00000000,
-//      /* */ 0x00000000,
-//      /* */ 0x00000000,
-//      /* */ 0x00000000,
-//      /* */ 0x00000000,
-//      /* */ 0x00000000,
-//      /* */ 0x00000000,
-//      /* */ 0x41900000,
-//      /* */ 0x3C23D70A,
-//      /* */ 0x04000000,
-//      /* */ 0x00000004,
-//      /* */ aSumClChest03_func,
-//  };
+FurnitureActorUnkStruct iam_sum_clchest03 = {
+    /* */ 0x015D0000,
+    /* */ 0x015D0F70,
+    /* */ 0x06000000,
+    /* */ 0x06000F70,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x00000000,
+    /* */ 0x41900000,
+    /* */ 0x3C23D70A,
+    /* */ 0x04000000,
+    /* */ 0x00000004,
+    /* */ &aSumClChest03_func,
+};
 
 // #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/furniture/ovl_sum_clchest03/ac_sum_clchest03/func_80A70C60_jp.s")
 extern BaseSkeletonR D_6000898;
 extern BaseAnimationR D_6000F5C;
 
-void func_80A70C60_jp(FurnitureActor* this, u8* data UNUSED) {
+void aSumClChest03_ct(FurnitureActor* this, u8* data UNUSED) {
     s32 pad[2];
     SkeletonInfoR* skeletonInfo = &this->skeletonInfo;
 
@@ -48,7 +53,7 @@ void func_80A70C60_jp(FurnitureActor* this, u8* data UNUSED) {
 }
 
 // #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/furniture/ovl_sum_clchest03/ac_sum_clchest03/func_80A70CF4_jp.s")
-void func_80A70CF4_jp(FurnitureActor *this, Actor *myroomactor, Game_Play *game_play, u8* data UNUSED)
+void aSumClChest03_mv(FurnitureActor *this, Actor *myroomactor, Game_Play *game_play, u8* data UNUSED)
 {
     if (common_data.clip.myRoomClip != NULL)
     {
@@ -60,7 +65,7 @@ void func_80A70CF4_jp(FurnitureActor *this, Actor *myroomactor, Game_Play *game_
 extern UNK_PTR D_6000020;
 extern UNK_PTR D_6000000;
 
-void func_80A70D3C_jp(FurnitureActor* this, Actor* myRoomActor UNUSED, Game_Play* game_play, u8* data UNUSED)
+void aSumClChest03_dw(FurnitureActor* this, Actor* myRoomActor UNUSED, Game_Play* game_play, u8* data UNUSED)
 {
     UNUSED s32 pad;
     s32 temp = game_play->state.frameCounter & 1;
@@ -93,7 +98,7 @@ void func_80A70D3C_jp(FurnitureActor* this, Actor* myRoomActor UNUSED, Game_Play
 }
 
 // #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/furniture/ovl_sum_clchest03/ac_sum_clchest03/func_80A70F50_jp.s")
-void func_80A70F50_jp(FurnitureActor* this UNUSED, u8* data UNUSED)
+void aSumClChest03_dt(FurnitureActor* this UNUSED, u8* data UNUSED)
 {
 
 }
