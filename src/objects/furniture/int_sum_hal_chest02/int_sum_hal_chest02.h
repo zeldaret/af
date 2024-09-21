80f4ff8bbbcf84002d088b929498b8e238f467bc
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-09-20 17:51:00 -0700

    extracted spooky wardrobe object

---
+++ src/objects/furniture/int_sum_hal_chest02/int_sum_hal_chest02.h
@@ -0,0 +1,25 @@
+#ifndef OBJECT_INT_SUM_HAL_CHEST02_H
+#define OBJECT_INT_SUM_HAL_CHEST02_H
+
+#include "gbi.h"
+#include "c_keyframe.h"
+
+extern u16 int_sum_hal_chest02_pal[];
+extern u8 int_sum_hal_chest02_hiki_tex_txt[];
+extern u8 int_sum_hal_chest02_hiite_tex_txt[];
+extern u8 int_sum_hal_chest02_top_tex_txt[];
+extern u8 int_sum_hal_chest02_front_tex_txt[];
+extern u8 int_sum_hal_chest02_back_tex_txt[];
+extern u8 int_sum_hal_chest02_kuki_tex_txt[];
+extern u8 cKF_ckcb_r_int_sum_hal_chest02_tbl[];
+extern s16 cKF_kn_int_sum_hal_chest02_tbl[];
+extern s16 cKF_c_int_sum_hal_chest02_tbl[];
+extern Keyframe cKF_ds_int_sum_hal_chest02_tbl[];
+extern BaseAnimationR cKF_ba_r_int_sum_hal_chest02;
+extern Vtx int_sum_hal_chest02_v[];
+extern Gfx int_sum_hal_chest01_door_model[];
+extern Gfx int_sum_hal_chest02_base_model[];
+extern JointElemR cKF_je_r_int_sum_hal_chest02_tbl[];
+extern BaseSkeletonR cKF_bs_r_int_sum_hal_chest02;
+
+#endif
--- src/overlays/furniture/ovl_sum_hal_chest02/ac_sum_hal_chest02.c
+++ src/overlays/furniture/ovl_sum_hal_chest02/ac_sum_hal_chest02.c
@@ -2,17 +2,18 @@
 #include "c_keyframe.h"
 #include "src/overlays/actors/ovl_My_Room/ac_my_room.h"
 #include "segment_symbols.h"
+#include "objects/furniture/int_sum_hal_chest02/int_sum_hal_chest02.h"
 
 void aSumHalChest02_ct(FurnitureActor* this, u8* data);
 void aSumHalChest02_mv(FurnitureActor* this, Actor* myRoomActor, Game_Play* game_play, u8* data);
-void aSumHalChest03_dw(FurnitureActor* this, Actor* myRoomActor, Game_Play* game_play, u8* data);
-void aSumHalChest03_dt(FurnitureActor* this, u8* data);
+void aSumHalChest02_dw(FurnitureActor* this, Actor* myRoomActor, Game_Play* game_play, u8* data);
+void aSumHalChest02_dt(FurnitureActor* this, u8* data);
 
 FurnitureActorFunctions aSumHalChest02_func = {
     /* */ aSumHalChest02_ct,
     /* */ aSumHalChest02_mv,
-    /* */ aSumHalChest03_dw,
-    /* */ aSumHalChest03_dt,
+    /* */ aSumHalChest02_dw,
+    /* */ aSumHalChest02_dt,
     /* */ NULL,
 };
 
@@ -40,9 +41,6 @@ FurnitureActorProfile iam_sum_hal_chest02 = {
     /* */ &aSumHalChest02_func,
 };
 
-extern BaseSkeletonR cKF_bs_r_int_sum_hal_chest02;
-extern BaseAnimationR cKF_ba_r_int_sum_hal_chest02;
-
 void aSumHalChest02_ct(FurnitureActor* this, u8* data UNUSED) {
     UNUSED s32 pad[2];
     SkeletonInfoR* skeletonInfo = &this->skeletonInfo;
@@ -60,10 +58,7 @@ void aSumHalChest02_mv(FurnitureActor* this, Actor* myRoomActor, Game_Play* game
     }
 }
 
-extern UNK_PTR D_6000020;
-extern UNK_PTR D_6000000;
-
-void aSumHalChest03_dw(FurnitureActor* this, Actor* myRoomActor UNUSED, Game_Play* game_play, u8* data UNUSED) {
+void aSumHalChest02_dw(FurnitureActor* this, Actor* myRoomActor UNUSED, Game_Play* game_play, u8* data UNUSED) {
     UNUSED s32 pad;
     s32 temp = game_play->state.frameCounter & 1;
     Mtx* mtx = &this->matrix[temp][0];
@@ -73,16 +68,16 @@ void aSumHalChest03_dw(FurnitureActor* this, Actor* myRoomActor UNUSED, Game_Pla
     if (1) {}
     gSPMatrix(POLY_OPA_DISP++, _Matrix_to_Mtx_new(game_play->state.gfxCtx),
               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-    gDPSetTextureImage(POLY_OPA_DISP++, G_IM_FMT_I, G_IM_SIZ_16b, 1, &D_6000020);
+    gDPSetTextureImage(POLY_OPA_DISP++, G_IM_FMT_I, G_IM_SIZ_16b, 1, &int_sum_hal_chest02_hiki_tex_txt);
     gDPSetTile(POLY_OPA_DISP++, G_IM_FMT_I, G_IM_SIZ_16b, 0, 0x0000, G_TX_LOADTILE, 0, G_TX_MIRROR | G_TX_WRAP, 7,
                G_TX_NOLOD, G_TX_MIRROR | G_TX_WRAP, 7, G_TX_NOLOD);
     gDPLoadSync(POLY_OPA_DISP++);
-    gDPLoadBlock(POLY_OPA_DISP++, G_TX_LOADTILE, 0, 0, 2047, 0);
+    gDPLoadBlock(POLY_OPA_DISP++, G_TX_LOADTILE, 0, 0, 0x7FF, 0);
     gDPPipeSync(POLY_OPA_DISP++);
     gDPSetTile(POLY_OPA_DISP++, G_IM_FMT_I, G_IM_SIZ_8b, 8, 0x0000, G_TX_RENDERTILE, 0, G_TX_MIRROR | G_TX_WRAP, 7,
                G_TX_NOLOD, G_TX_MIRROR | G_TX_WRAP, 7, G_TX_NOLOD);
     gDPSetTileSize(POLY_OPA_DISP++, G_TX_RENDERTILE, 0, 0, 0x00FC, 0x00FC);
-    gDPSetTextureImage(POLY_OPA_DISP++, G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, &D_6000000);
+    gDPSetTextureImage(POLY_OPA_DISP++, G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, &int_sum_hal_chest02_pal);
     gDPTileSync(POLY_OPA_DISP++);
     gDPSetTile(POLY_OPA_DISP++, G_IM_FMT_RGBA, G_IM_SIZ_4b, 0, 0x01F0, G_TX_LOADTILE, 0, G_TX_NOMIRROR | G_TX_WRAP,
                G_TX_NOMASK, G_TX_NOLOD, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMASK, G_TX_NOLOD);
@@ -94,5 +89,5 @@ void aSumHalChest03_dw(FurnitureActor* this, Actor* myRoomActor UNUSED, Game_Pla
     cKF_Si3_draw_R_SV(game_play, &this->skeletonInfo, mtx, NULL, NULL, NULL);
 }
 
-void aSumHalChest03_dt(FurnitureActor* this UNUSED, u8* data UNUSED) {
+void aSumHalChest02_dt(FurnitureActor* this UNUSED, u8* data UNUSED) {
 }
