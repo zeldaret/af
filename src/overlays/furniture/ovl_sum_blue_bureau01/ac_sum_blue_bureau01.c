1ede0109bcdf54ca939f1fca4aa967962810e786
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-09-25 12:43:18 -0700

    initial commit

---
+++ src/overlays/furniture/ovl_sum_blue_bureau01/ac_sum_blue_bureau01.c
@@ -0,0 +1,10 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/furniture/ovl_sum_blue_bureau01/ac_sum_blue_bureau01/func_80A4BC00_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/furniture/ovl_sum_blue_bureau01/ac_sum_blue_bureau01/func_80A4BC94_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/furniture/ovl_sum_blue_bureau01/ac_sum_blue_bureau01/func_80A4BCDC_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/furniture/ovl_sum_blue_bureau01/ac_sum_blue_bureau01/func_80A4BEF0_jp.s")
