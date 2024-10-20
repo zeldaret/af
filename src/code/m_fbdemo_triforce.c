5eacbe6245d26c27c771a5f3d0b6a3a9a330a1e8
Prakxo <lycos2003@gmail.com>
2024-10-20 15:42:50 +0200

    link triforce

---
+++ src/code/m_fbdemo_triforce.c
@@ -0,0 +1,18 @@
+#include "global.h"
+
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_fbdemo_triforce/fbdemo_triforce_startup.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_fbdemo_triforce/fbdemo_triforce_init.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_fbdemo_triforce/func_80083CE8_jp.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_fbdemo_triforce/fbdemo_triforce_move.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_fbdemo_triforce/fbdemo_triforce_draw.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_fbdemo_triforce/fbdemo_triforce_is_finish.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_fbdemo_triforce/fbdemo_triforce_settype.s")
+
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_fbdemo_triforce/fbdemo_triforce_setcolor_rgba8888.s")
