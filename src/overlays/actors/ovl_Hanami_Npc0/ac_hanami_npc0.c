2073bcc1b6fb683c9b0126d61a26a1a47f04ba3e
Kuchen mit Sahne <25233413+kuchenmitsahne@users.noreply.github.com>
2024-11-03 16:23:59 -0500

    Standard actor functions and aHM0_ct_data functions OK

--- src/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0.c
+++ src/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0.c
@@ -3,11 +3,17 @@
 #include "m_actor_dlftbls.h"
 #include "m_object.h"
 #include "overlays/gamestates/ovl_play/m_play.h"
+#include "m_demo.h"
+#include "macros.h"
+
+#define THIS ((Hanami_Npc0*)thisx)
 
 void aHM0_actor_ct(Actor* thisx, Game_Play* game_play);
 void aHM0_actor_dt(Actor* thisx, Game_Play* game_play);
 void aHM0_actor_init(Actor* thisx, Game_Play* game_play);
 void aHM0_actor_save(Actor* thisx, Game_Play* game_play);
+void func_809DE978_jp(Actor* thisx);
+void func_809DE948_jp(UNK_TYPE arg0);
 
 #if 0
 ActorProfile Hanami_Npc0_Profile = {
@@ -25,17 +31,44 @@ ActorProfile Hanami_Npc0_Profile = {
 };
 #endif
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0/aHM0_actor_ct.s")
+extern struct_809AEFA4 aHM0_ct_data;
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0/aHM0_actor_save.s")
+void aHM0_actor_ct(Actor* thisx, Game_Play* game_play) {
+    // TODO: import data
+    // static struct_809AEFA4 aHM0_ct_data = {
+    //     &aHM0_actor_move,
+    //     &aHM0_actor_draw,
+    //     4,
+    //     &aHM0_talk_request,
+    //     &func_809DEA20_jp,
+    //     aHM0_talk_end_chk,
+    // };
+    
+    Hanami_Npc0* this = THIS;
+    
+    if (common_data.clip.unk_040->unk_BC(thisx, game_play) == 1) {
+        this->unk7C0 = &func_809DE948_jp;
+        common_data.clip.unk_040->unk_C0(thisx, game_play, &aHM0_ct_data);
+    }
+}
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0/aHM0_actor_dt.s")
+void aHM0_actor_save(Actor *thisx, Game_Play *game_play) {
+    common_data.clip.unk_040->unk_C8(thisx, game_play);
+}
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0/aHM0_actor_init.s")
+void aHM0_actor_dt(Actor *thisx, Game_Play *game_play) {
+    common_data.clip.unk_040->unk_C4(thisx, game_play);
+}
+
+void aHM0_actor_init(Actor* thisx, Game_Play* game_play) {
+    common_data.clip.unk_040->unk_CC(thisx, game_play);
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0/func_809DE434_jp.s")
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0/func_809DE474_jp.s")
+void aHM0_actor_move(void) {
+    common_data.clip.unk_040->unk_D0();
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0/func_809DE4A0_jp.s")
 
@@ -71,10 +104,28 @@ ActorProfile Hanami_Npc0_Profile = {
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0/func_809DE978_jp.s")
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0/func_809DE9EC_jp.s")
+void aHM0_talk_request(Actor* thisx, UNK_TYPE arg1 UNUSED) {
+    mDemo_Request(7, thisx, func_809DE978_jp);
+}
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0/func_809DEA20_jp.s")
+// TODO: Possibly aHM0_talk_init, which exists in map, but type is different from function of same name
+// as found in ac_kamakura_npc0.c and ac_npc_engineer.c (both are void)
+s32 func_809DEA20_jp(UNK_TYPE arg0 UNUSED, UNK_TYPE arg1 UNUSED) {
+    mDemo_Set_ListenAble();
+    
+    return 1;
+}
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0/func_809DEA4C_jp.s")
+s32 aHM0_talk_end_chk(Actor* thisx, UNK_TYPE arg1 UNUSED) {
+    s32 result = FALSE;
+    
+    if (!mDemo_Check(7, thisx)) {
+        result = TRUE;
+    }
+    
+    return result;
+}
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0/func_809DEA8C_jp.s")
+void aHM0_actor_draw(void) {
+    common_data.clip.unk_040->unk_E4();
+}
--- src/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0.h
+++ src/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0.h
@@ -12,7 +12,9 @@ typedef void (*Hanami_Npc0ActionFunc)(struct Hanami_Npc0*, struct Game_Play*);
 
 typedef struct Hanami_Npc0 {
     /* 0x000 */ Actor actor;
-    /* 0x174 */ UNK_TYPE1 unk_174[0x7D4];
+    /* 0x174 */ UNK_TYPE1 unk_174[0x64C];
+    /* 0x7C0 */ UNK_PTR unk7C0;
+    /* 0x7C4 */ UNK_TYPE1 unk_7C4[0x184];
 } Hanami_Npc0; // size = 0x948
 
 #endif
