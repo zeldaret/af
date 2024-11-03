c14fc3ad60a24279d948ba03907a3ebe76898059
Kuchen mit Sahne <25233413+kuchenmitsahne@users.noreply.github.com>
2024-11-02 22:11:55 -0400

    Style/formatting tweaks

--- src/overlays/actors/ovl_Goza/ac_goza.c
+++ src/overlays/actors/ovl_Goza/ac_goza.c
@@ -6,7 +6,7 @@
 #include "overlays/actors/player_actor/m_player.h"
 #include "m_player_lib.h"
 #include "m_field_info.h"
-#include "69E2C0.h"
+#include "m_demo.h"
 #include "m_rcp.h"
 #include "macros.h"
 #include "gfx.h"
@@ -19,7 +19,7 @@ void aGOZ_actor_ct(Actor* thisx, Game_Play* game_play);
 void aGOZ_actor_dt(Actor* thisx, Game_Play* game_play);
 void aGOZ_actor_init(Actor* thisx, Game_Play* game_play);
 void aGOZ_actor_draw(Actor* thisx, Game_Play* game_play);
-void aGOZ_set_bgOffset(Goza* this, s32 processIndex);
+void aGOZ_set_bgOffset(Goza* this, s32 heightTableIndex);
 void aGOZ_setup_action(Goza* this, s32 processIndex);
 
 ActorProfile Goza_Profile = {
@@ -38,54 +38,55 @@ ActorProfile Goza_Profile = {
 
 extern Vtx obj_e_goza_shadow_v[];
 extern Gfx obj_e_goza_shadowT_model[];
-u8 aGOZ_shadow_vtx_fix_flg_table[] = { 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1,
-                                       1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1 };
-ShadowData aGOZ_shadow_data = { 24, aGOZ_shadow_vtx_fix_flg_table, 60.0f, &obj_e_goza_shadow_v, obj_e_goza_shadowT_model};
-
+u8 aGOZ_shadow_vtx_fix_flg_table[] = { 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1 };
+ShadowData aGOZ_shadow_data = { 24, aGOZ_shadow_vtx_fix_flg_table, 60.0f, obj_e_goza_shadow_v,
+                                obj_e_goza_shadowT_model };
 
 void aGOZ_actor_ct(Actor* thisx, Game_Play* game_play UNUSED) {
     Goza* this = THIS;
-    
+
     aGOZ_setup_action(this, 0);
     aGOZ_set_bgOffset(this, 1);
 }
 
 void aGOZ_actor_dt(Actor* thisx, Game_Play* game_play UNUSED) {
     Goza* this = THIS;
-    
+
     common_data.clip.structureClip->removeInstanceProc(common_data.clip.structureClip->objectSegmentTable,
                                                        ARRAY_COUNT(common_data.clip.structureClip->objectSegmentTable),
                                                        STRUCTURE_TYPE_GOZA, &this->structureActor.actor);
-    
+
     common_data.clip.structureClip->removeInstanceProc(common_data.clip.structureClip->paletteSegmentTable,
                                                        ARRAY_COUNT(common_data.clip.structureClip->paletteSegmentTable),
                                                        STRUCTURE_PALETTE_GOZA, &this->structureActor.actor);
-    
+
     common_data.clip.structureClip->removeInstanceProc(common_data.clip.structureClip->shadowSegmentTable,
                                                        ARRAY_COUNT(common_data.clip.structureClip->shadowSegmentTable),
                                                        STRUCTURE_TYPE_GOZA, &this->structureActor.actor);
 }
 
-void aGOZ_set_bgOffset(Goza* this, s32 processIndex) {
+void aGOZ_set_bgOffset(Goza* this, s32 heightTableIndex) {
+    // clang-format off
     static mCoBG_unkStruct2 height_table_ct[] = {
-        { 0x64, 0, 0, 0, 0, 0, 0},
-        { 0x64, 0, 0, 0, 0, 0, 0},
-        { 0x64, 0, 0, 0, 0, 0, 0},
-        { 0x64, 2, 2, 2, 2, 2, 0},
-        { 0x64, 2, 2, 2, 2, 2, 0},
-        { 0x64, 2, 2, 2, 2, 2, 0},
-        { 0x64, 0, 0, 0, 0, 0, 0},
-        { 0x64, 0, 0, 0, 0, 0, 0},
-        { 0x64, 0, 0, 0, 0, 0, 0}
+        { 0x64, 0, 0, 0, 0, 0, 0 },
+        { 0x64, 0, 0, 0, 0, 0, 0 },
+        { 0x64, 0, 0, 0, 0, 0, 0 },
+        { 0x64, 2, 2, 2, 2, 2, 0 },
+        { 0x64, 2, 2, 2, 2, 2, 0 },
+        { 0x64, 2, 2, 2, 2, 2, 0 },
+        { 0x64, 0, 0, 0, 0, 0, 0 },
+        { 0x64, 0, 0, 0, 0, 0, 0 },
+        { 0x64, 0, 0, 0, 0, 0, 0 }
     };
+    // clang-format on
     static mCoBG_unkStruct2* height_table[] = { height_table_ct, height_table_ct };
     static f32 addX[] = { -40.0f, 0.0f, 40.0f };
     static f32 addZ[] = { -40.0f, 0.0f, 40.0f };
 
     s32 i;
     xyz_t pos;
-    mCoBG_unkStruct2* offsetTable = height_table[processIndex];
-    
+    mCoBG_unkStruct2* offsetTable = height_table[heightTableIndex];
+
     for (i = 0; i < ARRAY_COUNT(addZ); i++) {
         pos.z = addZ[i] + this->structureActor.actor.home.pos.z;
 
@@ -118,11 +119,11 @@ void aGOZ_actor_move(Actor* thisx, Game_Play* game_play) {
     s32 gozaBlockZ;
     s32 playerBlockX;
     s32 playerBlockZ;
-    
+
     mFI_Wpos2BlockNum(&gozaBlockX, &gozaBlockZ, goza->actor.world.pos);
     mFI_Wpos2BlockNum(&playerBlockX, &playerBlockZ, player->actor.world.pos);
-    
-    if ((mDemo_Check(1, &player->actor) == 0) && (mDemo_Check(5, &player->actor) == 0) &&
+
+    if (!mDemo_Check(1, &player->actor) && !mDemo_Check(5, &player->actor) &&
         ((gozaBlockX != playerBlockX) || (gozaBlockZ != playerBlockZ))) {
         Actor_delete(&goza->actor);
     } else {
@@ -130,23 +131,23 @@ void aGOZ_actor_move(Actor* thisx, Game_Play* game_play) {
     }
 }
 
-void aGOZ_actor_init(Actor *thisx, Game_Play *game_play) {
+void aGOZ_actor_init(Actor* thisx, Game_Play* game_play) {
     Goza* this = THIS;
     StructureActor* goza = &this->structureActor;
-    
+
     mFI_SetFG_common(0xF0F4, goza->actor.home.pos, FALSE);
     aGOZ_actor_move(&goza->actor, game_play);
     goza->actor.update = aGOZ_actor_move;
 }
 
-void aGOZ_actor_draw(Actor *thisx, Game_Play *game_play) {
+void aGOZ_actor_draw(Actor* thisx, Game_Play* game_play) {
     GraphicsContext* gfxCtx = game_play->state.gfxCtx;
     u32 object = common_data.clip.structureClip->getObjectSegment(STRUCTURE_TYPE_GOZA);
     u16* palette = common_data.clip.structureClip->getPalSegment(STRUCTURE_PALETTE_GOZA);
-    Mtx *mtx =_Matrix_to_Mtx_new(gfxCtx);
+    Mtx* mtx = _Matrix_to_Mtx_new(gfxCtx);
 
     _texture_z_light_fog_prim_shadow(gfxCtx);
-    
+
     OPEN_SHADOW_DISP(gfxCtx);
     gSPSegment(__shadow_gfx++, G_MWO_SEGMENT_8, palette);
     gSegments[6] = (uintptr_t)OS_PHYSICAL_TO_K0(object);
@@ -154,13 +155,14 @@ void aGOZ_actor_draw(Actor *thisx, Game_Play *game_play) {
     gDPSetPrimColor(__shadow_gfx++, 0, 0x80, 255, 255, 255, 255);
 
     mtx = _Matrix_to_Mtx_new(gfxCtx);
+    
     if (mtx != NULL) {
         gSPMatrix(__shadow_gfx++, mtx, G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
         gSPDisplayList(__shadow_gfx++, goza_DL_model);
     }
-    
+
     CLOSE_SHADOW_DISP(gfxCtx);
-    
+
     if (mtx != NULL) {
         common_data.clip.unk_074->unk_04(game_play, &aGOZ_shadow_data, STRUCTURE_TYPE_GOZA);
     }
