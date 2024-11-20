3d4809d149216e98751e3036202f541cd3b9e62c
Kuchen mit Sahne <25233413+kuchenmitsahne@users.noreply.github.com>
2024-11-19 21:54:22 -0500

    Import data (44 differing words)

--- src/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0.c
+++ src/overlays/actors/ovl_Hanami_Npc0/ac_hanami_npc0.c
@@ -25,7 +25,6 @@ s32 func_809DEA20_jp(UNK_TYPE arg0 UNUSED, UNK_TYPE arg1 UNUSED);
 s32 aHM0_talk_end_chk(Actor* thisx, UNK_TYPE arg1 UNUSED);
 void aHM0_actor_draw(void);
 
-#if 0
 ActorProfile Hanami_Npc0_Profile = {
     /* */ ACTOR_HANAMI_NPC0,
     /* */ ACTOR_PART_NPC,
@@ -39,18 +38,15 @@ ActorProfile Hanami_Npc0_Profile = {
     /* */ (void*)none_proc1,
     /* */ aHM0_actor_save,
 };
-#endif
 
-// TODO: import data
-extern struct_809AEFA4 aHM0_ct_data;
-// struct_809AEFA4 aHM0_ct_data = {
-//     aHM0_actor_move,
-//     aHM0_actor_draw,
-//     4,
-//     aHM0_talk_request,
-//     func_809DEA20_jp,
-//     aHM0_talk_end_chk,
-// };
+struct_809AEFA4 aHM0_ct_data = {
+    aHM0_actor_move,
+    aHM0_actor_draw,
+    4,
+    aHM0_talk_request,
+    func_809DEA20_jp,
+    aHM0_talk_end_chk,
+};
 
 void aHM0_actor_ct(Actor* thisx, Game_Play* game_play) {
     Hanami_Npc0* this = THIS;
@@ -73,10 +69,8 @@ void aHM0_actor_init(Actor* thisx, Game_Play* game_play) {
     common_data.clip.unk_040->unk_CC(thisx, game_play);
 }
 
-extern UNK_TYPE4 D_809DEB00_jp[];
 void func_809DE434_jp(Actor* thisx, s32 index) {
-    // TODO: import data
-    // static UNK_TYPE4 D_809DEB00_jp[] = { 0x00000048, 0x0000004A, 0x00000049 };
+    static UNK_TYPE4 D_809DEB00_jp[] = { 0x00000048, 0x0000004A, 0x00000049 };
 
     common_data.clip.unk_040->unk_104(thisx, D_809DEB00_jp[index], 0, index);
 }
@@ -143,15 +137,12 @@ void func_809DE5BC_jp(Actor* thisx) {
     }
 }
 
-extern Hanami_Npc0_unk_93C D_809DEB0C_jp[];
-extern UNK_TYPE D_809DEB18_jp[];
-extern u8 D_809DEB30_jp[];
-extern f32 D_FLT_809DEB24_jp[];
 void func_809DE5DC_jp(Hanami_Npc0* this, s32 index) {
-    // static void* D_809DEB0C_jp[] = { func_809DE538_jp, func_809DE56C_jp, func_809DE5BC_jp };
-    // static UNK_TYPE D_809DEB18_jp[] = { 0x00000001, 0x00000002, 0x00000001 };
-    // static u8 D_809DEB30_jp[] = { 0x2F, 0x31, 0x32, 0x33 };
-    // static f32 D_FLT_809DEB24_jp[] = { 2.0f, 3.0f, 1.0f };
+    static Hanami_Npc0_unk_93C D_809DEB0C_jp[] = { func_809DE538_jp, func_809DE56C_jp, func_809DE5BC_jp };
+    static UNK_TYPE D_809DEB18_jp[] = { 0x00000001, 0x00000002, 0x00000001 };
+    static f32 D_FLT_809DEB24_jp[] = { 2.0f, 3.0f, 1.0f };
+    static u8 D_809DEB30_jp[] = { 0x2F, 0x31, 0x32, 0x33 };
+    
 
     this->unk_7C6 = 0;
     this->unk_938 = index;
@@ -177,22 +168,16 @@ void func_809DE6EC_jp(Hanami_Npc0* this, Game_Play* game_play UNUSED) {
     this->unk_93C(&this->actor);
 }
 
-extern Hanami_Npc0ActionFunc D_809DEB34_jp[];
-
 void func_809DE714_jp(Actor* thisx, Game_Play* game_play, s32 processIndex) {
     // TODO: confirm whether this is appropriate function type
-    // TODO: import data
-    // static Hanami_Npc0ActionFunc D_809DEB34_jp[] = { func_809DE6C8_jp, func_809DE6B4_jp, func_809DE6EC_jp };
+    static Hanami_Npc0ActionFunc D_809DEB34_jp[] = { func_809DE6C8_jp, func_809DE6B4_jp, func_809DE6EC_jp };
     Hanami_Npc0* this = THIS;
 
     (*D_809DEB34_jp[processIndex])(this, game_play);
 }
 
-extern UNK_TYPE D_809DEB40_jp[];
-
 void func_809DE744_jp(Hanami_Npc0* this, Game_Play* game_play UNUSED) {
-    // TODO: import data
-    // static UNK_TYPE D_809DEB40_jp[] = { 0x00000000, 0x00000001, 0x00000002 };
+    static UNK_TYPE D_809DEB40_jp[] = { 0x00000000, 0x00000001, 0x00000002 };
 
     if (this->unk_7C6 == 0xFF) {
         if (this->unk_7C5 == 0x12) {
@@ -212,20 +197,16 @@ void func_809DE7D0_jp(Hanami_Npc0* this, Game_Play* game_play UNUSED) {
     func_809DE4A0_jp(&this->actor);
 }
 
-extern Hanami_Npc0ActionFunc D_809DEB4C_jp[];
 // TODO: verify signatures of this and of functions in array
 void func_809DE800_jp(Actor* thisx, Game_Play* game_play, s32 index) {
-    // TODO: import data
-    // static Hanami_Npc0ActionFunc D_809DEB4C_jp[] = { func_809DE7D0_jp, func_809DE744_jp };
+    static Hanami_Npc0ActionFunc D_809DEB4C_jp[] = { func_809DE7D0_jp, func_809DE744_jp };
     Hanami_Npc0* this = THIS;
 
     (*D_809DEB4C_jp[index])(this, game_play);
 }
 
-extern s16 D_809DEB54_jp[];
 void func_809DE830_jp(Hanami_Npc0* this, Game_Play* game_play) {
-    // TODO: import data
-    // static s16 D_809DEB54_jp[4] = { 0x2000, 0xC000, -0x2000, 0x4000 };
+    static s16 D_809DEB54_jp[4] = { 0x2000, 0xC000, 0xE000, 0x4000 };
     s32 index = this->actor.fgName + 0xFFFF2FCE;
 
     this->unk_7A4 = &func_809DE800_jp;
@@ -255,21 +236,14 @@ void func_809DE8D0_jp(Hanami_Npc0* this, Game_Play* game_play) {
     func_809DE4C0_jp(&this->actor, game_play);
 }
 
-extern Hanami_Npc0ActionFunc D_809DEB5C_jp[];
-
 void func_809DE948_jp(Hanami_Npc0* this, Game_Play* game_play, s32 index) {
-    // TODO: import data
-    // static Hanami_Npc0ActionFunc D_809DEB5C_jp[] = { func_809DE830_jp, func_809DE8D0_jp };
+    static Hanami_Npc0ActionFunc D_809DEB5C_jp[] = { func_809DE830_jp, func_809DE8D0_jp };
 
     (*D_809DEB5C_jp[index])(this, game_play);
 }
 
-extern u32 D_809DEB64_jp[];
-
 void func_809DE978_jp(Actor* thisx) {
-    // TODO: import data
-    // TODO: last entry may just be padding
-    // static u32 D_809DEB64_jp[] = { 0x0000192D, 0x0000193C, 0x0000191E, 0x0000194B, 0x0000195A, 0x00001969, 0x00000000 };
+    static u32 D_809DEB64_jp[] = { 0x0000192D, 0x0000193C, 0x0000191E, 0x0000194B, 0x0000195A, 0x00001969 };
     Hanami_Npc0* this = THIS;
     enum NpcLooks looks = mNpc_GetNpcLooks(thisx);
 
