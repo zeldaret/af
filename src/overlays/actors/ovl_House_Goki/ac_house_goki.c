45c970ef2820ddd8d7d0f0f9e172375378375592
feacur <feacur@gmail.com>
2024-11-22 18:27:57 +0100

    aHG_actor_ct OK

--- src/overlays/actors/ovl_House_Goki/ac_house_goki.c
+++ src/overlays/actors/ovl_House_Goki/ac_house_goki.c
@@ -2,6 +2,7 @@
 #include "m_actor_dlftbls.h"
 #include "m_object.h"
 #include "overlays/gamestates/ovl_play/m_play.h"
+#include "m_actor_shadow.h"
 
 void aHG_actor_ct(Actor* thisx, Game_Play* game_play);
 void func_80A83770_jp(Actor* thisx, Game_Play* game_play);
@@ -15,6 +16,8 @@ void func_80A83A00_jp(House_Goki* this, Game_Play* game_play);
 void func_80A83A24_jp(House_Goki* this, Game_Play* game_play);
 void func_80A83D4C_jp(House_Goki* this, Game_Play* game_play);
 void func_80A8401C_jp(House_Goki* this, Game_Play* game_play);
+
+void func_80A8409C_jp(House_Goki* this, s32 arg1, Game_Play* game_play);
 
 ActorProfile House_Goki_Profile = {
     /* */ ACTOR_HOUSE_GOKI,
@@ -34,7 +37,23 @@ static House_GokiActionFunc init_process[] = { func_80A83930_jp, func_80A83994_j
 static House_GokiActionFunc process[] = { func_80A83A24_jp, func_80A83D4C_jp, func_80A8401C_jp };
 static s32 D_80A845DC_jp[] = { 0x060000C0, 0x06000188 };
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_House_Goki/ac_house_goki/aHG_actor_ct.s")
+// #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_House_Goki/ac_house_goki/aHG_actor_ct.s")
+void aHG_actor_ct(Actor* thisx, Game_Play* game_play) {
+    House_Goki* this = (House_Goki*)thisx;
+    this->unk_190 = 0x1E;
+    if (this->actor.params <= 0) {
+        this->actor.params = 0;
+        this->unk_190 = 0xFF;
+    }
+    this->actor.scale.x = 0.01f;
+    this->actor.scale.y = 0.01f;
+    this->actor.scale.z = 0.01f;
+    Shape_Info_init(&this->actor, 0, mAc_ActorShadowCircle, 6.0f, 6.0f);
+    xyz_t_move(&this->actor.home.pos, &this->actor.world.pos);
+    this->actor.update = aHG_actor_move;
+    this->actor.gravity = -2.0f;
+    func_80A8409C_jp(this, 1, game_play);
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_House_Goki/ac_house_goki/func_80A83770_jp.s")
 
