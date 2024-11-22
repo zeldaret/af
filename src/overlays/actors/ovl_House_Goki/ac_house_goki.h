52f00121f4ee3f636118567711ed7edaca1028a0
feacur <feacur@gmail.com>
2024-11-22 17:01:06 +0100

    prepare unknown size

--- src/overlays/actors/ovl_House_Goki/ac_house_goki.h
+++ src/overlays/actors/ovl_House_Goki/ac_house_goki.h
@@ -12,7 +12,7 @@ typedef void (*House_GokiActionFunc)(struct House_Goki*, struct Game_Play*);
 
 typedef struct House_Goki {
     /* 0x000 */ Actor actor;
-    /* 0x174 */ UNK_TYPE1 unk_174[0x28];
+    /* 0x174 */ UNK_TYPE1 unk_174[0x19C - 0x174];
 } House_Goki; // size = 0x19C
 
 #endif
