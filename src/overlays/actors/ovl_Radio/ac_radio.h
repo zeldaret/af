0bab7e592c3dcbd4207c2c57c06819ec2b7c1bfa
Leonid Kapitonov <Feacur@gmail.com>
2024-11-22 15:30:14 +0100

    `ac_radio` OK (#226)
    * prepare unknown size
    
    * move `.data` to the C side
    
    * aRAD_actor_ct OK
    
    * func_80A76B2C_jp OK
    
    * func_80A769E4_jp OK
    
    * aRAD_actor_init OK
    
    * func_80A76958_jp OK (rename to `aRAD_actor_dt`)
    
    * func_80A76A30_jp OK
    
    * func_80A76B4C_jp OK
    
    * aRAD_actor_draw OK
    
    * remove asm pragmas
    
    * document `ac_radio` actions
    
    * rename `func_80A769E4_jp` in accord with aKAG's
    
    * rename `func_80A76B4C_jp` -> `aRAD_actor_move`
    
    * use `StructureActor` as others do
    
    * apply review suggestions
    
    * seems to match too

--- src/overlays/actors/ovl_Radio/ac_radio.h
+++ src/overlays/actors/ovl_Radio/ac_radio.h
@@ -1,18 +1,20 @@
 #ifndef AC_RADIO_H
 #define AC_RADIO_H
 
-#include "ultra64.h"
 #include "m_actor.h"
-#include "unk.h"
+#include "overlays/actors/ovl_Structure/ac_structure.h"
 
 struct Game_Play;
 struct Radio;
 
+typedef enum RadioProcess {
+    RADIO_PROCESS_WAIT,
+} RadioProcess;
+
 typedef void (*RadioActionFunc)(struct Radio*, struct Game_Play*);
 
 typedef struct Radio {
-    /* 0x000 */ Actor actor;
-    /* 0x174 */ UNK_TYPE1 unk_174[0x164];
+    /* 0x000 */ StructureActor structureActor;
 } Radio; // size = 0x2D8
 
 #endif
