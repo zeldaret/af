fa87c6f941f0c4041141214babb8f70d4ac6fac5
Derek Hensley <hensley.derek58@gmail.com>
2024-12-25 19:32:40 -0800

    func_800D955C_jp

--- src/code/6FD190.c
+++ src/code/6FD190.c
@@ -1,6 +1,8 @@
 #include "global.h"
 
 #include "6FD190.h"
+#include "alignment.h"
+#include "libc64/malloc.h"
 
 uintptr_t func_800D94F0_jp(void) {
     B_80146070_jp = 0x80400000;
@@ -13,14 +15,37 @@ void func_800D9514_jp(void) {
 }
 
 void func_800D9524_jp(void) {
-    B_80146060_jp[0] = 0;
-    B_80146060_jp[1] = 0;
-    B_80146060_jp[2] = 0;
-    B_8014606C_jp = 0;
+    B_80146060_jp[0] = NULL;
+    B_80146060_jp[1] = NULL;
+    B_80146060_jp[2] = NULL;
+    B_8014606C_jp = NULL;
     func_800D9770_jp();
 }
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/6FD190/func_800D955C_jp.s")
+extern UNK_TYPE D_80000400; // linker segment?
+
+void func_800D955C_jp(s32 arg0) {
+    switch (arg0) {
+        case 0:
+            B_80146060_jp[0] = &D_80000400;
+            break;
+
+        case 1:
+            B_80146060_jp[1] = (void*)ALIGN64((uintptr_t)malloc_r(0x2583F));
+            break;
+
+        case 2:
+            B_8014606C_jp = (void*)ALIGN64((uintptr_t)malloc(0x2583F));
+            break;
+
+        case 3:
+            B_80146060_jp[2] = (void*)ALIGN64((uintptr_t)malloc(0x2583F));
+            break;
+
+        default:
+            break;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/code/6FD190/func_800D9618_jp.s")
 
