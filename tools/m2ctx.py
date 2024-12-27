e6943c64ceebf0e9f32871ed843c62565549f34e
Derek Hensley <hensley.derek58@gmail.com>
2024-12-27 09:03:50 -0800

    Fix m2ctx.py

--- tools/m2ctx.py
+++ tools/m2ctx.py
@@ -39,10 +39,10 @@ CPP_FLAGS = [
 def import_c_file(in_file) -> str:
     in_file = os.path.relpath(in_file, root_dir)
 
-    cpp_command = ["gcc", "-E", "-P", "-dD", *CPP_FLAGS, "-U__GNUC__", in_file]
+    cpp_command = ["gcc", "-E", "-P", "-undef", "-dD",  *CPP_FLAGS, in_file]
 
     with tempfile.NamedTemporaryFile(suffix=".c") as tmp:
-        stock_macros = subprocess.check_output(["gcc", "-E", "-P", "-dM", tmp.name], cwd=root_dir, encoding="utf-8")
+        stock_macros = subprocess.check_output(["gcc", "-E", "-P", "-undef", "-dM", tmp.name], cwd=root_dir, encoding="utf-8")
 
     try:
         out_text = subprocess.check_output(cpp_command, cwd=root_dir, encoding="utf-8")
